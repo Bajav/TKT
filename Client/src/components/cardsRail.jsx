3a62908b896e2b61d912ee2fd139624bc6916965
balijawa hussein <balijawahussein@gmail.com>
2024-10-12 12:04:05 +0300

    UPDATE : handling cards scroll effect

--- Client/src/components/cardsRail.jsx
+++ Client/src/components/cardsRail.jsx
@@ -1,20 +1,36 @@
-import React,{useRef} from 'react'
+import React,{useRef, useState} from 'react'
 import CardTitle from "./cardTitle";
 import Card from "./card";
+import Dummy from './dummyCard';
 
 function CardRail() {
-       
+       const itemsRef = useRef(null);
+       const [isMouseDown, setMouseDown] = useState(false);
+       const [startX, setStartX] = useState(0);
+       const [scrollLeft, setScrollLeft] = useState(0);
+
 
     
   return (
     <div className='cardRail'>
         <CardTitle countryName="africa"/>
-        <div className="cards">
+        <div className="cards"
+        ref={itemsRef}
+        onMouseDown={handleMouseDown}
+        onMouseEnter={handleMouseEnter}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        >
             <Card />
             <Card />
             <Card />
             <Card />
             <Card />
+            <Dummy />
+            <Dummy />
+            <Dummy />
+            <Dummy />
+            <Dummy />
         </div>
     </div>
   )
