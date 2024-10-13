3d03f1f664f75a30cc457beb6e8af81d3981f4b7
balijawa hussein <balijawahussein@gmail.com>
2024-10-13 15:42:50 +0300

    UPDATE : FLIGHS UI

---
+++ Client/src/components/flightSearch/calenderInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Calender(props) {
+  return (
+    <div className="calender flexInput">
+        <label htmlFor={props.label}>{props.labelName}</label>
+        <input type="text" name={props.inputName} id={props.ID} />
+    </div>
+  )
+}
+
+export default Calender;
