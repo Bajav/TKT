3f9206978f7b27240ede6d8750c37e1aa353dbda
balijawa hussein <balijawahussein@gmail.com>
2024-10-14 15:26:54 +0300

    CALENDER UI IS DONE

--- Client/src/components/flightSearch/calenderInputs.jsx
+++ Client/src/components/flightSearch/calenderInputs.jsx
@@ -3,7 +3,7 @@ import Calender from './calenderInput'
 
 function CalenderInputs() {
   return (
-    <div>
+    <div className='calendersUI'>
         <Calender label="date Of departure" labelName="date Of departure"  inputType="date" inputName="date Of departure"/>
         <Calender label="date Of Return" labelName="date Of Return"  inputType="date" inputName="date Of Return"/>
     </div>
