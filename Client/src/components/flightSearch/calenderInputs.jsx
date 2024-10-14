7b92e1d16ecf2a8d1b6c967be782fda3bcda9a5c
balijawa hussein <balijawahussein@gmail.com>
2024-10-14 15:14:48 +0300

    UPDATE : CALENDER UI

--- Client/src/components/flightSearch/calenderInputs.jsx
+++ Client/src/components/flightSearch/calenderInputs.jsx
@@ -5,7 +5,7 @@ function CalenderInputs() {
   return (
     <div>
         <Calender label="date Of departure" labelName="date Of departure"  inputType="date" inputName="date Of departure"/>
-        <Calender label="date Of departure" labelName="date Of departure"  inputType="date" inputName="date Of departure"/>
+        <Calender label="date Of Return" labelName="date Of Return"  inputType="date" inputName="date Of Return"/>
     </div>
   )
 }
