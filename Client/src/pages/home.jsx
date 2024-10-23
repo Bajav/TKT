8ded6ce536a1f591a104731dcfdce0d6760ec59e
balijawa hussein <balijawahussein@gmail.com>
2024-10-23 03:33:10 +0300

    UPDATE : KEY ADDED

--- Client/src/pages/home.jsx
+++ Client/src/pages/home.jsx
@@ -2,49 +2,10 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Home() {
-  const [value, setValue] = useState(""); 
-  const [status, setStatus] = useState(""); 
-  const [iataCodes, setIataCodes] = useState([]);
-  const [showDropdown, setShowDropdown] = useState(false);  
-
-  const handleChange = (e) => {
-    setValue(e.target.value);
-    setShowDropdown(true);  
-  }
-
-  const fetchIataCodes = async () => { 
-    try {
-      const response = await axios.get("http://localhost:3080/flights"); 
-      setStatus(response.statusText);
-      setIataCodes(response.data);
-      console.log(response);
-    } catch (err) {
-      console.log(err);
-      setStatus("400");
-    }
-  }
-
-  useEffect(() => {
-    fetchIataCodes();
-  }, []);
-
-  const filteredCodes = iataCodes.filter((code) => {
-    return (
-      code.AirportName.toLowerCase().includes(value.toLowerCase()) || 
-      code.AirportCode.toLowerCase().includes(value.toLowerCase()) || 
-      code.City.toLowerCase().includes(value.toLowerCase()) ||        
-      code.Country.toLowerCase().includes(value.toLowerCase())       
-    );
-  });
-
-  const onSearch = (searchTerm) => {
-    setValue(searchTerm); 
-    setShowDropdown(false);  
-  }
 
   return (
     <div className="testing">
-      
+      <h1>hello world</h1>
     </div>
   )
 }
