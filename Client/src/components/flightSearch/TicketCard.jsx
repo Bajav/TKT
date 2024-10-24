0d25a6250cb5a2abfa6df82743735d7c6dae5946
balijawa hussein <balijawahussein@gmail.com>
2024-10-24 17:42:45 +0300

    UPDATE : UI INTEGRATION , CITY SEARCH

--- Client/src/components/flightSearch/TicketCard.jsx
+++ Client/src/components/flightSearch/TicketCard.jsx
@@ -1,15 +1,15 @@
 import React, { Fragment, useEffect, useState } from "react";
-import { Arrow, CalenderSvg, TimeSvg } from "./flightArrowSvg";
+import { Arrow } from "./flightArrowSvg";
 import axios from "axios";
 
 function FlightCard() {
   const [flightResponse, setFlightResponse] = useState([]);
+  const [dropDown, showDropDown] = useState(null); 
 
   const fetchFlights = async () => {
     try {
       const res = await axios.get("http://localhost:3080/flights/flightsResults");
       setFlightResponse(res.data);
-      console.log()
     } catch (err) {
       console.log(err);
     }
@@ -19,51 +19,73 @@ function FlightCard() {
     fetchFlights();
   }, []);
 
+  // Button actions
+  const seeDetails = (index) => {
+    console.log("details button is clicked for card", index);
+    showDropDown((prevIndex) => (prevIndex === index ? null : index)); 
+  };
+
+  const bookNow = (e) => {
+    console.log("bookNow button is clicked");
+  };
+
   return (
     <Fragment>
-      {flightResponse.map((itinerary) => {
-        // console.log("results", itinerary);
-        return ( 
-          <div className="flights-res" key={itinerary.itineraries[0].segments[0].id || itinerary.id}>
-            <div className="flights-header">
-              <div className="airLineIcone">
-                <div className="icon"></div>
-                <h4>{itinerary.itineraries[0].segments[0].carrierCode}</h4>
-              </div>
-              <h4>{itinerary.itineraries[0].segments[0].aircraft.code}</h4>
-            </div>
-            <div className="ticket-header">
-              <div className="origin">
-                <h2>{itinerary.itineraries[0].segments[0].departure.iataCode}</h2>
-                <h5>Kampala, Uganda</h5>
-                <h5>{itinerary.itineraries[0].segments[0].departure.at.slice(11)}</h5>
-              </div>
-              <div className="item">
-                <Arrow color="#F5F7F8" />
-              </div>
-              <div className="item">
-                <h2>{itinerary.itineraries[0].segments[0].arrival.iataCode}</h2>
-                <h5>Dubai, UAE</h5>
-                <h5>{itinerary.itineraries[0].segments[0].arrival.at.slice(11)}</h5> {/* Corrected here */}
-              </div>
-            </div>
-            <div className="flights-actions">
-              <div className="time-details">
-                <div className="flex-tim">
-                  <h4>{itinerary.itineraries[0].segments[0].arrival.at.slice(0,10)}</h4>
+      {flightResponse.map((itinerary, index) => {  
+        const segments = itinerary.itineraries[0].segments;
+        const segmentNumber = segments.length;
+        const lastSegmentIndex = segmentNumber - 1;
+
+        return (
+          <div className="main-cards" key={itinerary.itineraries[0].segments[0].id || itinerary.id}>
+            <div className="flights-res">
+              <div className="flights-header">
+                <div className="airLineIcone">
+                  <div className="icon"></div>
+                  <h4>{segments[0].carrierCode}</h4>
                 </div>
-                <div className="flex-tim">
-                  <h4>{itinerary.itineraries[0].duration.slice(2)}</h4>
+                <h4>{segments[0].aircraft.code}</h4>
+              </div>
+              <div className="ticket-header">
+                <div className="origin">
+                  <h2>{segments[0].departure.iataCode}</h2>
+                  <h5>Kampala, Uganda</h5>
+                  <h5>{segments[0].departure.at.slice(11)}</h5>
+                </div>
+                <div className="center">
+                  <Arrow color="#F5F7F8" />
+                  {segmentNumber > 1 ? <h5>{segmentNumber - 1} stops</h5> : <h5>0 stops</h5>}
+                </div>
+                <div className="item">
+                  <h2>{segments[lastSegmentIndex].arrival.iataCode}</h2>
+                  <h5>Dubai, UAE</h5>
+                  <h5>{segments[lastSegmentIndex].arrival.at.slice(11)}</h5>
                 </div>
               </div>
-              <div className="price details">
-                <h4>${itinerary.price.grandTotal}/Pax</h4>
-              </div>
-              <div className="actions">
-                <button className="bookBtn">book now</button>
-                <button className="detailsBtn">see details</button>
+              <div className="flights-actions">
+                <div className="time-details">
+                  <div className="flex-tim">
+                    <h4>{segments[lastSegmentIndex].arrival.at.slice(0, 10)}</h4>
+                  </div>
+                  <div className="flex-tim">
+                    <h4>{itinerary.itineraries[0].duration.slice(2)}</h4>
+                  </div>
+                </div>
+                <div className="price details">
+                  <h4>${itinerary.price.grandTotal}</h4>
+                </div>
+                <div className="actions">
+                  <button onClick={bookNow} className="bookBtn">Book Now</button>
+                  <button onClick={() => seeDetails(index)} className="detailsBtn">See Details</button>
+                </div>
               </div>
             </div>
+            {dropDown === index ? (
+              <div className="flightsDetails">
+                <h3>Flight Details for Card {index + 1}:</h3>
+                <p>More information about the flight...</p>
+              </div>
+            ) : null}
           </div>
         );
       })}
