07c4d8077fd1fdf94f4d849bb1173e91d5671792
balijawa hussein <balijawahussein@gmail.com>
2024-11-17 03:16:43 +0300

    UPDATED : UPDATED LOADERS

--- Client/src/components/loader.jsx
+++ Client/src/components/loader.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function Loader(props) {
   return (
     <div className="main-loader">
-            <div className='loader'>
-            </div>
-            <h4>{props.loaderTag}</h4>
+      {/* <div className='loader'>
+            </div> */}
+      <div class="spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <h4>{props.loaderTag}</h4>
     </div>
-  )
+  );
 }
 
 export default Loader;
