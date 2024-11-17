7ff1527db925ca611c3283c93240bc11fc004fc4
balijawa hussein <balijawahussein@gmail.com>
2024-11-17 03:25:50 +0300

    UPDATE

--- Client/src/components/loader.jsx
+++ Client/src/components/loader.jsx
@@ -3,15 +3,15 @@ import React from "react";
 function Loader(props) {
   return (
     <div className="main-loader">
-      {/* <div className='loader'>
-            </div> */}
-      <div class="spinner">
+      <div className='loader'>
+            </div>
+      {/* <div class="spinner">
         <div></div>
         <div></div>
         <div></div>
         <div></div>
         <div></div>
-      </div>
+      </div> */}
       <h4>{props.loaderTag}</h4>
     </div>
   );
