4fdac6a7e0cabde926ee69be543824c247025d95
balijawa hussein <balijawahussein@gmail.com>
2024-11-18 00:04:06 +0300

    UPDATE : CHANING UI

--- Client/src/pages/flightPricing.jsx
+++ Client/src/pages/flightPricing.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react';
+import axios from "axios";
+
+
 
 function FlightPricing() {
+    const [res, setRes] = useState([]);
+
+    const fetchFlightPricing = async ()=>
+        {
+            try{
+            const response = await axios.get("http://localhost:3000/flights/flightsResults/flightPricing");
+            console.log(response.data);
+            setRes(response.data);
+        }
+             catch(err){
+                console.log(err);
+            }
+        }
+
+        useEffect(()=>{
+            fetchFlightPricing();
+        },[])
   return (
     <div>
       pricing
