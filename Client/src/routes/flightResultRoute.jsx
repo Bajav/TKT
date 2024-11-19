285ad3d4f3c34b8fff6024d8307ff379cdedbc90
balijawa hussein <balijawahussein@gmail.com>
2024-11-19 18:04:10 +0300

    UPDATE: CRASH DETECTED

--- Client/src/routes/flightResultRoute.jsx
+++ Client/src/routes/flightResultRoute.jsx
@@ -1,10 +1,13 @@
 import {Routes , Route} from 'react-router-dom';
 import FlightResult from '../pages/flightResults';
+import FlightPricing from '../pages/flightPricing';
 
 const FlightsRoutes =()=>{
     return(
     <Routes>
-        <Route path='flights/flightsResults' element={<FlightResult />} />
+        <Route path='flights/flightsResults' element={<FlightResult />} >
+        <Route path="flights/flightsResults/flightPricing" element={<FlightPricing />}/>
+        </Route>
     </Routes>
     );
 };
