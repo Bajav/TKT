9da308ac5ebb4eff3110c73985340a97f2093284
Balijawa Hussein <balijawahussein@gmail.com>
2025-04-12 20:06:58 +0300

    UPDATE : START TRAVELER PAGE

--- Client/src/pages/TravelerForm/TravelerForm.jsx
+++ Client/src/pages/TravelerForm/TravelerForm.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import { Arrow } from "../../components/flightSearch/flightArrowSvg";
+import BackBTN from "../../components/features/BackButton/BackBTN";
+import PaxForm from "../../components/features/PaxForm/PaxForm";
 
 function TravelerForm() {
   return (
-    <div>TravelerForm</div>
-  )
+    <section className="travelForm">
+      <BackBTN to={"/"} btnName="back" />
+      <h1>Enter Traveler Details</h1>
+      <div className="FlightResult-header">
+        <div className="ticket-header">
+          <div className="origin">
+            <h2>Kla</h2>
+            <h3>Kampala, Uganda</h3>
+          </div>
+          <div className="item">
+            <Arrow color="#313030" width="200px" />
+          </div>
+          <div className="item">
+            <h2>DXB</h2>
+            <h3>Dubai , UAE</h3>
+          </div>
+        </div>
+      </div>
+      <PaxForm />
+    </section>
+  );
 }
 
-export default TravelerForm
+export default TravelerForm;
