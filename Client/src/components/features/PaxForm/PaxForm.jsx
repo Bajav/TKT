b6b3b90fb17bc71bd08cf2f526855e32221c7206
Balijawa Hussein <balijawahussein@gmail.com>
2025-04-12 21:20:32 +0300

    CREATE : PHONE INPUT VALIDATOR

--- Client/src/components/features/PaxForm/PaxForm.jsx
+++ Client/src/components/features/PaxForm/PaxForm.jsx
@@ -3,8 +3,8 @@ import { useNavigate } from "react-router-dom";
 import FlexInput from "../../../props/FlexInput/FlexInput";
 
 function PaxForm() {
-    // define navigation
-    const navigate = useNavigate();
+  // define navigation
+  const navigate = useNavigate();
   // defineState
   const [inputs, setInputs] = useState({});
   // handle form changes
@@ -16,15 +16,65 @@ function PaxForm() {
   // handle form submits
   const handleSubmit = (e) => {
     e.preventDefault();
-    navigate("/")
+    navigate("/");
   };
 
-  return( 
-  <form className="paxForm">
-    <div className="names">
-        <FlexInput For="fName" labelName="first name" placeholder="enter first name"/>
-    </div>
-  </form>);
+  return (
+    <form className="paxForm">
+      <div className="names">
+        <FlexInput
+          name="fName"
+          type="text"
+          For="fName"
+          labelName="first name"
+          placeholder="enter first name"
+        />
+        <FlexInput
+          name="lName"
+          type="text"
+          For="lName"
+          labelName="last name"
+          placeholder="enter last name"
+        />
+      </div>
+      <div className="fulls">
+        <FlexInput name="DOB" type="date" For="DOB" labelName="date of birth" />
+        <FlexInput
+          name="Nationality"
+          type="text"
+          For="Nationality"
+          labelName="Nationality"
+          placeholder="enter Nationality"
+        />
+        <FlexInput
+          name="otherName"
+          type="text"
+          For="otherName"
+          labelName="Other Names"
+          placeholder="enter otehr names"
+        />
+        <h4>passport details</h4>
+        <FlexInput
+          name="IssuanceDate"
+          type="date"
+          For="Issuance date"
+          labelName="IssuanceDate"
+        />
+        <FlexInput
+          name="expiryDate"
+          type="date"
+          For="expiry date"
+          labelName="expiry Date"
+        />
+        <FlexInput name="" type="" For="" labelName=" " placeholder="  " />
+      </div>
+      <div className="cell">
+        <FlexInput name="" type="" For="" labelName=" " placeholder="  " />
+        <FlexInput name="" type="" For="" labelName=" " placeholder="  " />
+      </div>
+      <FlexInput name="" type="" For="" labelName=" " placeholder="  " />
+    </form>
+  );
 }
 
 export default PaxForm;
