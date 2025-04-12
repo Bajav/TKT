0cb35081131a7aa68ac1335916ca24f1671de64c
Balijawa Hussein <balijawahussein@gmail.com>
2025-04-12 19:16:31 +0300

    CANCEL BUTTON

--- Client/src/components/features/BackButton/BackBTN.jsx
+++ Client/src/components/features/BackButton/BackBTN.jsx
@@ -4,7 +4,9 @@ import { useNavigate } from 'react-router-dom'
 function BackBTN({btnName = "back",to = -1}) {
     const navigate = useNavigate();
   return (
-    <button className='backBtn' onClick={()=>{navigate(to)}}>{btnName}</button>
+    <div className='flexButtons'>
+      <button  onClick={()=>{navigate(to)}}>{btnName}</button>
+    </div>
   )
 }
 
