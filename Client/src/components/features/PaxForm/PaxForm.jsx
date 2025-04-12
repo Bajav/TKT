4d7cb846e75d938f9cee41443947bd1cfabee4c4
Balijawa Hussein <balijawahussein@gmail.com>
2025-04-12 21:37:02 +0300

    UPDATE : PHONE

--- Client/src/components/features/PaxForm/PaxForm.jsx
+++ Client/src/components/features/PaxForm/PaxForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import FlexInput from "../../../props/FlexInput/FlexInput";
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
 
 function PaxForm() {
   // define navigation
@@ -20,60 +22,73 @@ function PaxForm() {
   };
 
   return (
-    <form className="paxForm">
-      <div className="names">
-        <FlexInput
-          name="fName"
-          type="text"
-          For="fName"
-          labelName="first name"
-          placeholder="enter first name"
-        />
-        <FlexInput
-          name="lName"
-          type="text"
-          For="lName"
-          labelName="last name"
-          placeholder="enter last name"
-        />
-      </div>
-      <div className="fulls">
-        <FlexInput name="DOB" type="date" For="DOB" labelName="date of birth" />
-        <FlexInput
-          name="Nationality"
-          type="text"
-          For="Nationality"
-          labelName="Nationality"
-          placeholder="enter Nationality"
-        />
-        <FlexInput
-          name="otherName"
-          type="text"
-          For="otherName"
-          labelName="Other Names"
-          placeholder="enter otehr names"
-        />
-        <h4>passport details</h4>
-        <FlexInput
-          name="IssuanceDate"
-          type="date"
-          For="Issuance date"
-          labelName="IssuanceDate"
-        />
-        <FlexInput
-          name="expiryDate"
-          type="date"
-          For="expiry date"
-          labelName="expiry Date"
-        />
+    <div className="paxFormContainer">
+      <form className="paxForm">
+        <div className="names">
+          <FlexInput
+            name="fName"
+            type="text"
+            For="fName"
+            labelName="first name"
+            placeholder="enter first name"
+          />
+          <FlexInput
+            name="lName"
+            type="text"
+            For="lName"
+            labelName="last name"
+            placeholder="enter last name"
+          />
+        </div>
+        <div className="fulls">
+          <FlexInput
+            name="DOB"
+            type="date"
+            For="DOB"
+            labelName="date of birth"
+          />
+          <FlexInput
+            name="Nationality"
+            type="text"
+            For="Nationality"
+            labelName="Nationality"
+            placeholder="enter Nationality"
+          />
+          <FlexInput
+            name="otherName"
+            type="text"
+            For="otherName"
+            labelName="Other Names"
+            placeholder="enter otehr names"
+          />
+          <h4>passport details</h4>
+          <FlexInput
+            name="IssuanceDate"
+            type="date"
+            For="Issuance date"
+            labelName="IssuanceDate"
+          />
+          <FlexInput
+            name="expiryDate"
+            type="date"
+            For="expiry date"
+            labelName="expiry Date"
+          />
+          <FlexInput
+            name="DocumentType"
+            type="text"
+            For="Document Type"
+            labelName="Document Type"
+            placeholder=" enter Document Type "
+          />
+        </div>
+        <div className="cell">
+          <PhoneInput placeholder="enter phone number" name="phone number" />
+          <FlexInput name="" type="" For="" labelName=" " placeholder="  " />
+        </div>
         <FlexInput name="" type="" For="" labelName=" " placeholder="  " />
-      </div>
-      <div className="cell">
-        <FlexInput name="" type="" For="" labelName=" " placeholder="  " />
-        <FlexInput name="" type="" For="" labelName=" " placeholder="  " />
-      </div>
-      <FlexInput name="" type="" For="" labelName=" " placeholder="  " />
-    </form>
+      </form>
+    </div>
   );
 }
 
