277890c6f2a445e426a1891e427434d08dc5f328
Balijawa Hussein <balijawahussein@gmail.com>
2025-04-27 21:33:48 +0300

    UPDATE : COMMIT

--- Client/src/components/flightSearch/ClickOption.jsx
+++ Client/src/components/flightSearch/ClickOption.jsx
@@ -2,19 +2,18 @@ import React from "react";
 
 function ClickOption(props) {
   return (
-<div className="flex flex-col items-center justify-center">
-  <label className="relative flex flex-col items-center justify-center cursor-pointer">
-    <span className="z-20 mb-2">{props.label}</span> 
-    <input
-      type="checkbox"
-      name={props.checkName}
-      onChange={props.changeFunc}
-      checked={props.checkedName}
-      className="absolute w-full h-full opacity-20 cursor-pointer"
-    />
-  </label>
-</div>
-
+    <div className="flex flex-col items-center justify-center">
+      <label className="relat" htmlFor={props.labelName}>
+        {props.label}
+      </label>
+      <input
+        className="relative z-10 "
+        type="checkbox"
+        name={props.checkName}
+        onChange={props.changeFunc}
+        checked={props.checkedName}
+      />
+    </div>
   );
 }
 
