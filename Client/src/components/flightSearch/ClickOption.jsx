9dbb20eb8c7a8bf2b7ce8b608b019279a4b1b2bd
Balijawa Hussein <balijawahussein@gmail.com>
2025-04-27 19:31:25 +0300

    UPDATE : STYLING THE BUTTON COMPONENT

--- Client/src/components/flightSearch/ClickOption.jsx
+++ Client/src/components/flightSearch/ClickOption.jsx
@@ -2,18 +2,19 @@ import React from "react";
 
 function ClickOption(props) {
   return (
-    <div className="flex flex-col">
-      <label className="relative z-20 " htmlFor={props.labelName}>
-        {props.label}
-      </label>
-      <input
-        className="z-10"
-        type="relative checkbox"
-        name={props.checkName}
-        onChange={props.changeFunc}
-        checked={props.checkedName}
-      />
-    </div>
+<div className="flex flex-col items-center justify-center">
+  <label className="relative flex flex-col items-center justify-center cursor-pointer">
+    <span className="z-20 mb-2">{props.label}</span> 
+    <input
+      type="checkbox"
+      name={props.checkName}
+      onChange={props.changeFunc}
+      checked={props.checkedName}
+      className="absolute w-full h-full opacity-0 cursor-pointer"
+    />
+  </label>
+</div>
+
   );
 }
 
