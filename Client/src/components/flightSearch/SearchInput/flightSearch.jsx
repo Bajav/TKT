a65da93b5b82fb36aa8ed8c30122ce1c1a2917d1
Balijawa Hussein <balijawahussein@gmail.com>
2025-05-25 00:31:26 +0300

    UPDATE

--- Client/src/components/flightSearch/SearchInput/flightSearch.jsx
+++ Client/src/components/flightSearch/SearchInput/flightSearch.jsx
@@ -1,5 +1,6 @@
-import React, { Fragment, useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
+import './flightSearch.scss';
 
 function FlightSearchInput(props) {
   // state
