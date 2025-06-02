55c9af1c30a05968e12a3d12e9effe395d2b5067
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-02 03:15:34 +0300

    UPDATE : ADDING FLIGHT RESULTS CONTEXT

--- Client/src/components/context/flightSearch.context.jsx
+++ Client/src/components/context/flightSearch.context.jsx
@@ -1,19 +1,30 @@
-import {createContext, useState } from "react";
+import { createContext, useState } from "react";
 
 export const FlightContext = createContext({
   flightSearch: null,
   setFormData: () => null,
-  iataCodes:[],
-  setIataCodes:()=>{},
-  airlineData:[],
-  setAirlineData:()=>{}
+  iataCodes: [],
+  setIataCodes: () => {},
+  airlineData: [],
+  setAirlineData: () => {},
+  flightResults:[],
+  setFlightResults:()=>{}
 });
 
 export const FlightSearchProvider = ({ children }) => {
   const [flightSearch, setFormData] = useState(null);
   const [iataCodes, setIataCodes] = useState([]);
-    const [airlineData, setAirlineData] = useState([]);
-  const value = { flightSearch, setFormData,iataCodes, setIataCodes,airlineData, setAirlineData };
+  const [airlineData, setAirlineData] = useState([]);
+  const [flightResults, setFlightResults] = useState([]);
+  const value = {
+    flightSearch,
+    setFormData,
+    iataCodes,
+    setIataCodes,
+    airlineData,
+    setAirlineData,
+    flightResults, setFlightResults
+  };
   return (
     <FlightContext.Provider value={value}>{children}</FlightContext.Provider>
   );
