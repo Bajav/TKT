b57a1cc17d064ec6f459c825e5f74159f159dc28
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-02 16:41:34 +0300

    UPDATE : UI

--- Client/src/components/flightSearch/Ticket/TicketCard.jsx
+++ Client/src/components/flightSearch/Ticket/TicketCard.jsx
@@ -218,25 +218,25 @@ function FlightCard() {
                     className="airline-logo"
                   />
                 </div>
-                <h4>{"" || ""}</h4>
+                <h4>{"UR, Uganda Air" || ""}</h4>
               </div>
-              <h4>{"" || ""}</h4>
+              <h4>{"" || "economy premium"}</h4>
             </div>
 
             <div className="ticket-header">
               <div className="origin">
-                <h2>{"" || ""}</h2>
-                <h5>{"" || "xxx"}</h5>
-                <h5>{"" || ""}</h5>
+                <h2>{"Kla" || ""}</h2>
+                <h5>{"Kampala, Uganda" || ""}</h5>
+                <h5>{"12:00 pm" || ""}</h5>
               </div>
               <div className="center">
                 <Arrow color="#F5F7F8" width="200px" />
                 {2 > 1 ? <h5>{2 - 1} stops</h5> : <h5>0 stops</h5>}
               </div>
               <div className="item">
-                <h2>{"" || ""}</h2>
-                <h5>{"" || ""}</h5>
-                <h5>{"" || ""}</h5>
+                <h2>{"DXB" || ""}</h2>
+                <h5>{"Dubai , UAE" || ""}</h5>
+                <h5>{"2.00pm" || ""}</h5>
               </div>
             </div>
 
