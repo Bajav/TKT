a93c04cf0d7b04f06c138e760a31e624477fc5c6
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-07 04:09:39 +0300

    UPDATE : FLIGHT REVIEW CSS

--- Client/src/pages/flightPricing/flightPricing.jsx
+++ Client/src/pages/flightPricing/flightPricing.jsx
@@ -29,200 +29,59 @@ function FlightPricing() {
     fetchFlightPricing();
     // console.log("Updated res:", res);
   }, []);
-// define navigation
+  // define navigation
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <section className="reviewFlight">
       <BackBTN to={"/"} btnName="cancel" />
       <h1>Review Flight</h1>
-      <div className="mainReview">
-        <div className="departureReview">
-          <h1>departure flight</h1>
-
-          <div className="ticket-header">
-            <div className="origin">
-              <h2>kla</h2>
-              <h5>Kampala, Uganda</h5>
-              <h5>12:00 pm</h5>
-              <h5>TERMINAL : 1E</h5>
-            </div>
-            <div className="center">
-              <Arrow color="#F5F7F8" width="200px" />
-            </div>
-            <div className="item">
-              <h2>dxb</h2>
-              <h5>Dubai , UAE</h5>
-              <h5>2.00pm</h5>
-              <h5>TERMINAL : 1E</h5>
-            </div>
+      <section className="taxes-container">
+        <div className="taxes">
+          <div className="taxesHeader">
+            <h5>fare breakdown </h5>
+            <h5>taxes breakdown </h5>
           </div>
-
-          <div className="flightData">
-            <h5>AIRLINE : KQ A380 </h5>
-            <h5>CO2 : 93 KG</h5>
-            <h5>THU 05-DEC-24</h5>
-            <h5>CLASS : R ECONOMY</h5>
-          </div>
-          <div className="infoHighlights">
-            <h5> CARRY ON INCLUDED </h5>
-            <h5>NO STOPS</h5>
-            <h5>LAYOVER : 3HR40MINS</h5>
-            <h5>CHECKED BAGS : 2 PCS</h5>
-          </div>
-        </div>
-        <div className="departureReview">
-          <h1>destination flight</h1>
-
-          <div className="ticket-header">
-            <div className="origin">
-              <h2>kla</h2>
-              <h5>Kampala, Uganda</h5>
-              <h5>12:00 pm</h5>
-              <h5>TERMINAL : 1E</h5>
+          <Border />
+          <div className="taxesBody">
+            <div className="bodyOne">
+              <h4>Base: $230</h4>
+              <h4>Taxes: $330</h4>
+              <h4>Total: $560</h4>
             </div>
-            <div className="center">
-              <Arrow color="#F5F7F8" width="200px" />
-            </div>
-            <div className="item">
-              <h2>dxb</h2>
-              <h5>Dubai , UAE</h5>
-              <h5>2.00pm</h5>
-              <h5>TERMINAL : 1E</h5>
+            <div className="bodyOne">
+              <h4>F6 YQ - Carrier-Imposed Surcharge: $10</h4>
+              <h4>UL - Passenger Service Charges: $10</h4>
+              <h4>AE - UAE Passenger Service Charge: $10</h4>
+              <h4>F6 - UAE Passenger Facility Charge: $10</h4>
+              <h4>UG - Security Charge: $10</h4>
+              <h4>TP - UAE Passenger Security and Safety Fee: $10</h4>
             </div>
           </div>
-
-          <div className="flightData">
-            <h5>AIRLINE : KQ A380 </h5>
-            <h5>CO2 : 93 KG</h5>
-            <h5>THU 05-DEC-24</h5>
-            <h5>CLASS : R ECONOMY</h5>
-          </div>
-          <div className="infoHighlights">
-            <h5> CARRY ON INCLUDED </h5>
-            <h5>NO STOPS</h5>
-            <h5>LAYOVER : 3HR40MINS</h5>
-            <h5>CHECKED BAGS : 2 PCS</h5>
-          </div>
-        </div>
-      </div>
-      <section className="taxes">
-        <Border />
-        <div className="taxesHeader">
-          <h5>fare breakdown </h5>
-          <h5>taxes breakdown </h5>
-        </div>
-        <Border />
-        <div className="taxesBody">
-          <div className="bodyOne">
-            <h4>Base: $230</h4>
-            <h4>Taxes: $330</h4>
-            <h4>Total: $560</h4>
-          </div>
-          <div className="bodyOne">
-            <h4>F6 YQ - Carrier-Imposed Surcharge: $10</h4>
-            <h4>UL - Passenger Service Charges: $10</h4>
-            <h4>AE - UAE Passenger Service Charge: $10</h4>
-            <h4>F6 - UAE Passenger Facility Charge: $10</h4>
-            <h4>UG - Security Charge: $10</h4>
-            <h4>TP - UAE Passenger Security and Safety Fee: $10</h4>
-          </div>
-        </div>
-        <div className="paxData">
-              <div className="paxDataContainer">
-                <div className="data">
-                  <h4>Travelers: 4</h4>
-                </div>
-                <div className="pax">
-                  <h4>Adults: 1</h4>
-                </div>
-                <div className="pax">
-                  <h4>Children: 1</h4>
-                </div>
-                <div className="pax">
-                  <h4>Infants: 0</h4>
-                </div>
+          <div className="paxData">
+            <div className="paxDataContainer">
+              <div className="data">
+                <h4>Travelers: 4</h4>
               </div>
-            </div> 
+              <div className="pax">
+                <h4>Adults: 1</h4>
+              </div>
+              <div className="pax">
+                <h4>Children: 1</h4>
+              </div>
+              <div className="pax">
+                <h4>Infants: 0</h4>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
-      <button className="continueBtn" onClick={()=>navigate("/travelerData")}>Continue</button>
+      <button className="continueBtn" onClick={() => navigate("/travelerData")}>
+        Continue
+      </button>
     </section>
   );
 }
 
 export default FlightPricing;
-
-// CLEAN
-// {confirmOder.flightOffers.map((flightOffer, offerInd) => (
-//   <div className="mainReview" key={`offer-${offerInd}`}>
-//     {flightOffer.itineraries.map((itinerary, itinInd) => (
-//       <div className="departureReview" key={`itin-${itinInd}`}>
-//         {itinerary.segments.map((segment, segInd) => (
-//           <div className="ticket" key={`seg-${segInd}`}>
-//             <div className="ticket-header">
-//               <div className="origin text-review">
-//                 <h2>{segment.departure.iataCode}</h2>
-//                 <h5>{segment.departure.city || "Unknown City"}</h5>
-//                 <h5>{new Date(segment.departure.at).toLocaleTimeString()}</h5>
-//                 <h6>TERMINAL: {segment.departure.terminal || "N/A"}</h6>
-//               </div>
-//               <div className="center">
-//                 <Arrow color="#F5F7F8" width="200px" />
-//                 <h4>{segment.duration || "N/A"}</h4>
-//               </div>
-//               <div className="departure text-review">
-//                 <h2>{segment.arrival.iataCode}</h2>
-//                 <h5>{segment.arrival.city || "Unknown City"}</h5>
-//                 <h5>{new Date(segment.arrival.at).toLocaleTimeString()}</h5>
-//                 <h6>TERMINAL: {segment.arrival.terminal || "N/A"}</h6>
-//               </div>
-//             </div>
-//             <div className="flight-info">
-//               <div className="flex-info">
-//                 <h5>AIRLINE: {segment.carrierCode} {segment.aircraft.code}</h5>
-//               </div>
-//               <div className="flex-info">
-//                 <h5>NO STOPS</h5>
-//               </div>
-//               <div className="flex-info">
-//                 <h5>CO2: {segment.co2Emissions?.[0]?.weight || "N/A"} {segment.co2Emissions?.[0]?.weightUnit || ""}</h5>
-//               </div>
-//               <div className="flex-info">
-//                 <h5>{new Date(segment.departure.at).toLocaleDateString()}</h5>
-
-//               </div>
-//               <div className="flex-info">
-//                 <h5>CLASS: {"" || "N/A"} {flightOffer.travelerPricings[0]?.fareDetailsBySegment[0]?.cabin || "N/A"}</h5>
-//               </div>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     ))}
-
-{
-}
-{
-  /* <div className="paxData">
-              <div className="paxDataContainer">
-                <div className="data">
-                  <h4>Travelers: 4</h4>
-                </div>
-                <div className="pax">
-                  <h4>Adults: 1</h4>
-                </div>
-                <div className="pax">
-                  <h4>Children: 1</h4>
-                </div>
-                <div className="pax">
-                  <h4>Infants: 0</h4>
-                </div>
-              </div>
-            </div> */
-}
-
-//   <div className="taxesHeader">
-//   <h1>Fare Breakdown</h1>
-//   <h1>Taxes Breakdown</h1>
-// </div>
