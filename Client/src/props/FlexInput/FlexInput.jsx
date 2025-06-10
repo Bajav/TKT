7ff2eef23dd17e5f49ace60712a730197b5860b9
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-10 15:38:07 +0300

    UPDATE : UI

--- Client/src/props/FlexInput/FlexInput.jsx
+++ Client/src/props/FlexInput/FlexInput.jsx
@@ -2,7 +2,7 @@ function FlexInput(props) {
   return (
     <div className='LabelInput'>
         <label htmlFor={props.For}>{props.labelName}</label>
-        <input type={props.type} placeholder={props.placeholder} name={props.name} onChange={props.change}/>
+        <input required type={props.type} placeholder={props.placeholder}  autoComplete="off" name={props.name} onChange={props.change}/>
     </div>
   )
 }
