2a058f408bc35b895f231318152539dea2d80b7e
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-11 06:43:42 +0300

    GSAP IS LIVE

--- Client/src/components/bearLoader/bearLoader.component.jsx
+++ Client/src/components/bearLoader/bearLoader.component.jsx
@@ -1,37 +1,52 @@
 import "./bearLoader.styles.scss";
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, useRef } from "react";
 import { FlightContext } from "../../components/context/flightSearch.context";
 import errorIcon from "../../assets/icons/error-svgrepo-com.svg";
-import { motion, scale } from "motion/react";
+import { gsap } from "gsap";
+
 function BearLoader() {
   const { upsellError } = useContext(FlightContext);
   const [errorCode, setErrorCode] = useState(false);
-  console.log(upsellError);
+  const errorRef = useRef(null);
+
   useEffect(() => {
-    upsellError?.status === 400 ? setErrorCode(true) : setErrorCode(false);
+    setErrorCode(upsellError?.status === 400);
   }, [upsellError]);
+
+  useEffect(() => {
+    if (errorCode && errorRef.current) {
+      gsap.fromTo(
+        errorRef.current,
+        {
+          y: 200,
+          scale: -0,
+          opacity: 0,
+        },
+        {
+          y: 0,
+          scale: 1,
+          opacity: 1,
+          duration: 1,
+          ease: "power3.out",
+        }
+      );
+    }
+  }, [errorCode]);
 
   return (
     <div className="loadingClass">
       <span className="loaded"></span>
-   
-        {errorCode ? (
-          <motion.div
-            aria-live="assertive"
-            initial={{ opacity: 0, x: -100 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -100 }} // ✅ Enables exit animation
-            transition={{ type: "spring", stiffness: 300, damping: 20 }}
-            className="uppsellError"
-          >
-            <img src={errorIcon} alt="Error Icon" />
-            <h1>{upsellError?.detail || "Something went wrong."}</h1>
-          </motion.div>
-        ) : (
-          <div className="finding-deals">
-            <h1>Finding deals...</h1>
-          </div>
-        )}
+
+      {errorCode ? (
+        <div ref={errorRef} className="uppsellError">
+          <img src={errorIcon} alt="Error Icon" />
+          <h1>{upsellError?.detail || "Something went wrong."}</h1>
+        </div>
+      ) : (
+        <div className="finding-deals">
+          <h1>Finding deals...</h1>
+        </div>
+      )}
     </div>
   );
 }
