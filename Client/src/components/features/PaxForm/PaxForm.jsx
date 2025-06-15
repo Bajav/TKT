69792e7a45d02cf0e78c97edc8f6de2f0b837be5
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-15 22:35:23 +0300

    UPDATE : UI

--- Client/src/components/features/PaxForm/PaxForm.jsx
+++ Client/src/components/features/PaxForm/PaxForm.jsx
@@ -32,8 +32,13 @@ function PaxForm() {
         <FlexInput  labelName="last name" value={inputs.lName || " "} type="text" placeholder="input last name" name="lName" change={handleChange} />
         <FlexInput  labelName="email" value={inputs.email || " "} type="email" placeholder="input email address" name="email" change={handleChange} />
         <FlexInput  labelName="phone number" value={inputs.pNumber || " "} type="tel" placeholder="input phone number" name="pNumber" change={handleChange} />
-        <FlexInput  labelName="" value={inputs.name || " "} type="" placeholder="" name="" change={handleChange} />
-        <FlexInput  labelName="" value={inputs.name || " "} type="" placeholder="" name="" change={handleChange} />
+        <FlexInput  labelName="gender" value={inputs.gender || " "} type="" placeholder="input gender" name="gender" change={handleChange} />
+        <FlexInput  labelName="date of birth" value={inputs.DOB || " "} type="date" placeholder="input date of birth" name="DOB" change={handleChange} />
+        <FlexInput  labelName="Docoment Type" value={inputs.docType || " "} type="text" placeholder="input docment type" name="docType" change={handleChange} />
+        <FlexInput  labelName="document number" value={inputs.docNumber || " "} type="text" placeholder="input document number" name="docNumber" change={handleChange} />
+        <FlexInput  labelName="document issuance country" value={inputs.issuanceCountry || " "} type="text" placeholder="input issuance country" name="issuanceCountry" change={handleChange} />
+        <FlexInput  labelName="date of issuance" value={inputs.dateIssuance || " "} type="date" placeholder="input date issuance" name="dateIssuance" change={handleChange} />
+        <FlexInput  labelName="date of expiry" value={inputs.dateExpiry || " "} type="date" placeholder="input date dateExpiry" name="dateExpiry" change={handleChange} />
         <button>submit</button>
       </form>
     </div>
