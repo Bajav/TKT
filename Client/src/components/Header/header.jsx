361cb58e722589b75403b5282ae401f8eb1828dd
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-18 23:19:30 +0300

    UPDATE : UI

--- Client/src/components/Header/header.jsx
+++ Client/src/components/Header/header.jsx
@@ -1,10 +1,12 @@
 import "./headr.styles.scss";
 import LocationComponent from "../LOCATION/location.component";
 import { useState, useContext, useEffect } from "react";
-import { motion } from "motion/react";
+import { motion, AnimatePresence } from "motion/react";
 import popUpSignIn from "../../Controllers/signinwithpopup.controller";
 import { UserContext } from "../context/user.context";
 import defaultProfile from "../../assets/icons/profile-default-svgrepo-com.svg";
+import createUserFromAuth from "../../Controllers/createUser.controller";
+
 const LocationHeader = () => {
   const { userData, setUserData } = useContext(UserContext);
   const [dropDown, setDropDown] = useState(false);
@@ -15,8 +17,9 @@ const LocationHeader = () => {
     try {
       const signinedInUser = await popUpSignIn();
       setUserData(signinedInUser.user);
-      console.log("✅ User signed in:", signinedInUser.user);
+      createUserFromAuth(signinedInUser.user);
       setSigninedIn(true);
+      console.log("✅ User signed in:", userData);
     } catch (err) {
       console.log("❌ Error signing in:", err);
     }
@@ -29,7 +32,6 @@ const LocationHeader = () => {
 
   return (
     <div className="header">
-      {console.log(userData?.photoURL)}
       <LocationComponent />
       <div onClick={showDropdown} className="icon">
         <img
@@ -42,42 +44,44 @@ const LocationHeader = () => {
           }}
         />
       </div>
-
-      {dropDown && (
-        <motion.div
-          initial={{ opacity: 0, height: 0, width: 0 }}
-          animate={{ opacity: 1, height: 175, width: 200 }}
-          transition={{ delay: 0.2, duration: 0.5, type: "tween" }}
-          className="user-drop-down"
-        >
-          {signedIn ? (
-            <motion.div
-              initial={{ opacity: 0, scale: 0.9 }}
-              animate={{ opacity: 1, scale: 1 }}
-              transition={{ delay: 0.2, duration: 0.3 }}
-              className="drop-down-contents"
-            >
-              <div className="user-drop-down-header">
-                <h3>{userData?.displayName.toLocaleUpperCase()}</h3>
-                <p>{userData?.email}</p>
+      <AnimatePresence>
+        {dropDown && (
+          <motion.div
+            initial={{ opacity: 0, height: 0, width: 0 }}
+            animate={{ opacity: 1, height: 175, width: 200 }}
+            exit={{ opacity: 0, height: 0, width: 0 }}
+            transition={{ delay: 0.2, duration: 0.5, type: "tween" }}
+            className="user-drop-down"
+          >
+            {signedIn ? (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.9 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{ delay: 0.2, duration: 0.3 }}
+                className="drop-down-contents"
+              >
+                <div className="user-drop-down-header">
+                  <h3>{userData?.displayName.toLocaleUpperCase()}</h3>
+                  <p>{userData?.email}</p>
+                </div>
+                <ul className="drop-down-links">
+                  <li>your flights</li>
+                  <li>likes</li>
+                  <li>community</li>
+                  <li>log out</li>
+                </ul>
+              </motion.div>
+            ) : (
+              <div className="signIn-container">
+                <div className="display">
+                  <p>sign in / up to view console</p>
+                </div>
+                <button onClick={signinwithpopup}>sign in</button>
               </div>
-              <ul className="drop-down-links">
-                <li>your flights</li>
-                <li>likes</li>
-                <li>community</li>
-                <li>log out</li>
-              </ul>
-            </motion.div>
-          ) : (
-            <div className="signIn-container">
-              <div className="display">
-                <p>sign in / up to view console</p>
-              </div>
-              <button onClick={signinwithpopup}>sign in</button>
-            </div>
-          )}
-        </motion.div>
-      )}
+            )}
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
