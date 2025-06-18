97db316dc49d9f2de141e905faf9ba76ccc19a8b
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-18 14:10:27 +0300

    UPDATE : UI

--- Client/src/components/Header/header.jsx
+++ Client/src/components/Header/header.jsx
@@ -1,12 +1,26 @@
-import './headr.styles.scss';
-import LocationComponent from '../LOCATION/location.component';
+import "./headr.styles.scss";
+import LocationComponent from "../LOCATION/location.component";
 
- function LocationHeader() {
+function LocationHeader() {
   return (
-    <div className='header'>
+    <div className="header-container">
+      <div className="header">
         <LocationComponent />
         <div className="icon"></div>
+      </div>
+      <div className="user-drop-down">
+        <div className="user-drop-down-header">
+          <h3>balijawa hussein</h3>
+          <p>balijawahussein@gmail.com</p>
+        </div>
+        <ul className="drop-down-links">
+          <li>your flights</li>
+          <li>likes</li>
+          <li>community</li>
+          <li>log in</li>
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 export default LocationHeader;
