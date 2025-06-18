61b911983ad9f9c8f3d1c0ecdc5ba2e21bd2cfe9
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-18 15:32:43 +0300

    UPDATE : DROP UI

--- Client/src/components/Header/header.jsx
+++ Client/src/components/Header/header.jsx
@@ -1,23 +1,35 @@
 import "./headr.styles.scss";
 import LocationComponent from "../LOCATION/location.component";
+import { useState } from "react";
+import { motion } from "motion/react";
 
 function LocationHeader() {
+  const [dropDown, setDropDown] = useState(true);
   return (
-      <div className="header">
-        <LocationComponent />
-      <div className="user-drop-down">
-        <div className="icon"></div>
-        <div className="user-drop-down-header">
-          <h3>balijawa hussein</h3>
-          <p>balijawahussein@gmail.com</p>
-        </div>
-        <ul className="drop-down-links">
-          <li>your flights</li>
-          <li>likes</li>
-          <li>community</li>
-          <li>log out</li>
-        </ul>
-      </div>
+    <div className="header">
+      <LocationComponent />
+      <div className="icon"></div>
+      {dropDown && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2, duration: 2, type: "spring" }}
+          className="user-drop-down"
+        >
+          <motion.div className="drop-down-contents">
+            <div className="user-drop-down-header">
+              <h3>balijawa hussein</h3>
+              <p>balijawahussein@gmail.com</p>
+            </div>
+            <ul className="drop-down-links">
+              <li>your flights</li>
+              <li>likes</li>
+              <li>community</li>
+              <li>log out</li>
+            </ul>
+          </motion.div>
+        </motion.div>
+      )}
     </div>
   );
 }
