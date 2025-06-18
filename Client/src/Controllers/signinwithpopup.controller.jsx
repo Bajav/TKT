99544d088f2027e3988f1a9c455939e1cb075e2c
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-18 17:55:07 +0300

    UPDATE : POP UP SIGN IN

--- Client/src/Controllers/signinwithpopup.controller.jsx
+++ Client/src/Controllers/signinwithpopup.controller.jsx
@@ -2,3 +2,5 @@ import { signInWithPopup } from "firebase/auth";
 import { provider, auth } from "../Utils/firebase.utils";
 
 const popUpSignIn = () => signInWithPopup(auth,provider);
+
+export default popUpSignIn;
