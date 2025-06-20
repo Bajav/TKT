17c63ec1a495910b5a0355a6146fa9a937edf6ae
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-20 11:07:45 +0300

    UPDATE : CREATE A SUCCESS PAGE

--- Client/src/pages/pasengerInputPage/passengers.jsx
+++ Client/src/pages/pasengerInputPage/passengers.jsx
@@ -1,15 +1,14 @@
-import { useContext,useState } from "react";
+import { useContext, useState } from "react";
 import "./passenger.styles.scss";
 import TicketHeader from "../../components/flightSearch/Ticket/ticketheader.component";
 import PaxForm from "../../components/features/PaxForm/PaxForm";
 import Checkout from "../../components/Model/checkout.model";
 import { motion } from "motion/react";
 import { UiContext } from "../../components/context/ui.context";
-
+import { Fragment } from "react";
 
 function Passengers() {
-  const {isModel} = useContext(UiContext);
-  // const [isModel,setModel]= useState(true)
+  const { isModel, isSuccess } = useContext(UiContext);
   return (
     <section className="passengers">
       <h1>enter traveller details</h1>
@@ -39,8 +38,16 @@ function Passengers() {
           transition={{ delay: 0.2, type: "tween", duration: 0.5 }}
           className="model-overlay"
         >
-          <h1>enter card details to book the flight</h1>
-          <Checkout />
+          {isSuccess ? (
+            <motion.div className="check-out-container">
+              <h1>enter card details to book the flight</h1>
+              <Checkout />
+            </motion.div>
+          ) : (
+            <motion.div className="succes-container">
+              <Checkout />
+            </motion.div>
+          )}
         </motion.div>
       )}
     </section>
