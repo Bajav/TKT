b3defaa491bddac031ca70e1a30f179e56421b93
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-20 10:59:20 +0300

    UPDATE : IS SUCCESS

--- Client/src/components/context/ui.context.jsx
+++ Client/src/components/context/ui.context.jsx
@@ -3,12 +3,15 @@ import { createContext, useState } from "react";
 const UiContext = createContext({
   isModel: true,
   setModel: () => {},
+  isSuccess: false,
+  setSuccess: () => {},
 });
 
 const UiContextProvider = ({ children }) => {
   const [isModel, setModel] = useState(false);
-  const value = { isModel, setModel };
+  const [isSuccess, setSuccess] = useState(false);
+  const value = { isModel, setModel, isSuccess, setSuccess };
   return <UiContext.Provider value={value}>{children}</UiContext.Provider>;
 };
 
-export {UiContext,UiContextProvider};
+export { UiContext, UiContextProvider };
