5d6e599fd401d32a062c97e6253c2e7f2dbdd876
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-20 11:35:05 +0300

    UPDATE : SUCCESS UI IS DONE

--- Client/src/components/CheckoutSuccess/success.component.jsx
+++ Client/src/components/CheckoutSuccess/success.component.jsx
@@ -1,14 +1,16 @@
 import "./success.styles.scss";
-import successIcon from '../../assets/icons/white-heavy-check-mark-svgrepo-com.svg'
+import successIcon from "../../assets/icons/white-heavy-check-mark-svgrepo-com.svg";
 
 function SuccessfulPayment() {
   return (
     <div className="success-payment-container">
-      <h3>payment successful</h3>
-      <p>
-        Flight successfully booked you can find your flight ticket in the email
-        or check your console to see ticket.
-      </p>
+      <div className="text-container">
+        <h3>payment successful</h3>
+        <p>
+          Flight successfully booked you can find your <br /> flight ticket in the
+          email or check your <br /> console to see ticket.
+        </p>
+      </div>
       <div className="success-icon-anime">
         <img src={successIcon} alt="success-checkMark" />
       </div>
