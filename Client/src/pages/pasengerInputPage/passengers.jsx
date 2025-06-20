c53f90296c1eaeddc9e593928a1975474ea5a2bf
Balijawa Hussein <balijawahussein@gmail.com>
2025-06-20 10:00:25 +0300

    UPDATE : MODEL OVERLAY ANIMATION

--- Client/src/pages/pasengerInputPage/passengers.jsx
+++ Client/src/pages/pasengerInputPage/passengers.jsx
@@ -3,11 +3,10 @@ import "./passenger.styles.scss";
 import TicketHeader from "../../components/flightSearch/Ticket/ticketheader.component";
 import PaxForm from "../../components/features/PaxForm/PaxForm";
 import Checkout from "../../components/Model/checkout.model";
+import { motion } from "motion/react";
 
 function Passengers() {
-  const [isModel,setModel]= useState(false);
-
-
+  const [isModel, setModel] = useState(false);
 
   return (
     <section className="passengers">
@@ -22,12 +21,25 @@ function Passengers() {
         arrowColor="#222"
       />
 
-     {isModel ? (<div className="form">
-        <PaxForm />
-      </div>):(
-        <div className="model-overlay">
+      {isModel ? (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2, type: "tween", duration: 0.5 }}
+          className="form"
+        >
+          <PaxForm />
+        </motion.div>
+      ) : (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 0.2, type: "tween", duration: 0.5 }}
+          className="model-overlay"
+        >
+          <h1>enter card details to book the flight</h1>
           <Checkout />
-        </div>
+        </motion.div>
       )}
     </section>
   );
