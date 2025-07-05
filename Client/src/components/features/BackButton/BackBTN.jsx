58a3867d193ae6a097a6185bd5870f641fb130fe
Balijawa Hussein <balijawahussein@gmail.com>
2025-07-06 02:07:33 +0300

    UPDATE : UI

--- Client/src/components/features/BackButton/BackBTN.jsx
+++ Client/src/components/features/BackButton/BackBTN.jsx
@@ -1,11 +1,18 @@
 import { useNavigate } from 'react-router-dom'
 import "./backBtn.scss";
+import { ArrowLeft } from 'lucide-react';
+import { useState } from 'react';
 
 function BackBTN({btnName = "back",to = -1,onClick}) {
     const navigate = useNavigate();
+    const [style,setStyles]= useState(false);
+    const changeColor ={
+      color :"#3e3e3e"
+    }
+
   return (
     <div className='flexButtons'>
-      <button  onClick={onClick}>{btnName}</button>
+      <button  onClick={onClick}>{<ArrowLeft style={style ? {changeColor}:{color:"#fff"}}  onClick={()=>setStyles(true)} size={20}/>}</button>
     </div>
   )
 }
