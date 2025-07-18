f3e232d3a589981e82f6daebc9847871af005645
Balijawa Hussein <balijawahussein@gmail.com>
2025-07-18 22:51:48 +0300

    UPDATE : DATE TYPE COMPONENT PROP

--- Client/src/components/date-trip-container/tripdate.component.jsx
+++ Client/src/components/date-trip-container/tripdate.component.jsx
@@ -1,1 +1,14 @@
 import './tripdate.styles.scss';
+import straightBoarder from '../../assets/icons/90deg.svg'
+
+function TripDateContainer({tripType,dates}) {
+  return (
+    <div className='container'>
+        <h3>{tripType}</h3>
+        <img src={straightBoarder} />
+        <h3>{dates}</h3>
+    </div>
+  )
+}
+
+export default TripDateContainer
