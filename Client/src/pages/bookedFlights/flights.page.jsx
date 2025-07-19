53ecab732e2c7e84bb19b52c5f33581b04dbb7de
Balijawa Hussein <balijawahussein@gmail.com>
2025-07-20 00:34:17 +0300

    UPDATE : UI

--- Client/src/pages/bookedFlights/flights.page.jsx
+++ Client/src/pages/bookedFlights/flights.page.jsx
@@ -1,11 +1,12 @@
 import './flights.style.scss';
 import BookedTicket from '../../components/BookedFlightTicket/ticket.component';
 
-import React from 'react'
 
 function BookedFlights() {
   return (
-    <section className='tickets-page'></section>
+    <section className='tickets-page'>
+      <BookedTicket />
+    </section>
   )
 }
 
