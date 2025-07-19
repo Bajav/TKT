64f925c9b7c6fa2e9d90752ff7d36278133b7195
Balijawa Hussein <balijawahussein@gmail.com>
2025-07-20 01:26:26 +0300

    UPDATE : BOOKED TICKET UI

--- Client/src/components/BookedFlightTicket/ticket.component.jsx
+++ Client/src/components/BookedFlightTicket/ticket.component.jsx
@@ -1,9 +1,35 @@
-import './ticket.styles.scss';
+import "./ticket.styles.scss";
+import TicketHeader from "../flightSearch/Ticket/ticketheader.component";
 
 function BookedTicket() {
+  const defaults = {
+    color: "#1e1e1e",
+  };
   return (
-    <div className='booked-ticket'>BookedTicket</div>
-  )
+    <div className="booked-ticket">
+      <div className="booked-ticket-header">
+        <div className="airlinelogo"></div>
+        <div className="airline-data">
+          <div className="flexed-text">
+            <h3>KL 920</h3>
+            <h3>A232/FL</h3>
+          </div>
+        </div>
+      </div>
+      <TicketHeader
+        styles={defaults}
+        originCode="kla"
+        originCity="kampala, uganda"
+        originTime="08:12:00"
+        originTerminal="terminal : 1e"
+        arrowColor="#222"
+        destinationCode="dxb"
+        destinationCity="dubai,uae"
+        destinationTime="08:12:00"
+        destinationTerminal="terminal : 2b"
+      />
+    </div>
+  );
 }
 
-export default BookedTicket
+export default BookedTicket;
--- Client/src/components/flightSearch/Ticket/ticketheader.component.jsx
+++ Client/src/components/flightSearch/Ticket/ticketheader.component.jsx
@@ -2,27 +2,28 @@ import "./ticket.scss";
 import { Arrow } from "../flightArrowSvg";
 
 function TicketHeader(props) {
+  
   return (
     <div className="ticket-header">
       <div className="origin">
-        <h2>{props.originCode}</h2>
-        <h5>{props.originCity}</h5>
-        <h5>{props.originTime}</h5>
-        <h5>{props.originTerminal}</h5>
+        <h2 style={props.styles}>{props.originCode}</h2>
+        <h5 style={props.styles}>{props.originCity}</h5>
+        <h5 style={props.styles} >{props.originTime}</h5>
+        <h5 style={props.styles} >{props.originTerminal}</h5>
       </div>
       <div className="center">
         <Arrow color={props.arrowColor} width="200px" />
         {props.segmentNum > 1 ? (
-          <h5>{props.segment - 1} stops</h5>
+          <h5 style={props.styles}>{props.segment - 1} stops</h5>
         ) : (
           <h5>0 stops</h5>
         )}
       </div>
       <div className="item">
-        <h2>{props.destinationCode}</h2>
-        <h5>{props.destinationCity}</h5>
-        <h5>{props.destinationTime}</h5>
-        <h5>{props.destinationTerminal}</h5>
+        <h2 style={props.styles}>{props.destinationCode}</h2>
+        <h5 style={props.styles}>{props.destinationCity}</h5>
+        <h5 style={props.styles}>{props.destinationTime}</h5>
+        <h5 style={props.styles}>{props.destinationTerminal}</h5>
       </div>
     </div>
   );
