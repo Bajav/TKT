5e29b9fdb99fe09d4eae199475b89caf964ee431
Balijawa Hussein <balijawahussein@gmail.com>
2025-07-20 14:04:05 +0300

    UPDATE : TICKET UI

--- Client/src/components/BookedFlightTicket/ticket.component.jsx
+++ Client/src/components/BookedFlightTicket/ticket.component.jsx
@@ -1,5 +1,5 @@
 import "./ticket.styles.scss";
-import Barcode from 'react-barcode'
+import Barcode from "react-barcode";
 import TicketHeader from "../flightSearch/Ticket/ticketheader.component";
 import klmLogo from "../../assets/images/klmLogo.png";
 import border from "../../assets/icons/border-line.svg";
@@ -8,26 +8,26 @@ function BookedTicket() {
     color: "#1e1e1e",
   };
   const barcodeOptions = {
-  width: 2,
-  height: 50,
-  format: "CODE128",
-  displayValue: false,
-  fontOptions: "",
-  font: "monospace",
-  textAlign: "center",
-  textPosition: "bottom",
-  textMargin: 2,
-  fontSize: 20,
-  background: "transparent",
-  lineColor: "#222",
-  margin: 0,
-  marginTop: undefined,
-  marginBottom: undefined,
-  marginLeft: undefined,
-  marginRight: undefined,
-  id: undefined,
-  className: undefined
-}
+    width: 2,
+    height: 50,
+    format: "CODE128",
+    displayValue: false,
+    fontOptions: "",
+    font: "monospace",
+    textAlign: "center",
+    textPosition: "bottom",
+    textMargin: 2,
+    fontSize: 20,
+    background: "transparent",
+    lineColor: "#222",
+    margin: 0,
+    marginTop: undefined,
+    marginBottom: undefined,
+    marginLeft: undefined,
+    marginRight: undefined,
+    id: undefined,
+    className: undefined,
+  };
   return (
     <div className="booked-ticket">
       <div className="booked-ticket-header">
@@ -66,31 +66,41 @@ function BookedTicket() {
           <h3>passenger</h3>
           <h3>balijawa hussein</h3>
         </div>
-        <div className="detail-flex">
-          <h3>ticket number</h3>
-          <h3>FL/ETK 2123347218</h3>
-        </div>
+
         <div className="detail-flex">
           <h3>cabin class</h3>
           <h3>economy</h3>
-        </div>
-        <div className="detail-flex">
-          <h3>flight code</h3>
-          <h3>a380 KQC3</h3>
         </div>
         <div className="detail-flex">
           <h3>seat number</h3>
           <h3>18A</h3>
         </div>
         <div className="detail-flex">
+          <h3>ticket number</h3>
+          <h3>FL/ETK 2123347218</h3>
+        </div>
+
+        <div className="detail-flex">
+          <h3>flight code</h3>
+          <h3>a380 KQC3</h3>
+        </div>
+
+        <div className="detail-flex">
           <h3>Terminal</h3>
           <h3>T1</h3>
         </div>
       </div>
       <div className="ticket-border">
-      <img src={border} />
+        <img src={border} />
       </div>
-      <Barcode height={barcodeOptions.height} margin={barcodeOptions.margin} displayValue={barcodeOptions.displayValue} background={barcodeOptions.background} lineColor={barcodeOptions.lineColor} value="212334721921233492334233423211" />
+      <Barcode
+        height={barcodeOptions.height}
+        margin={barcodeOptions.margin}
+        displayValue={barcodeOptions.displayValue}
+        background={barcodeOptions.background}
+        lineColor={barcodeOptions.lineColor}
+        value="212334721921233492334233423211"
+      />
     </div>
   );
 }
