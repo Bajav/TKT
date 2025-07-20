2aac590ad71af2919dfb65ba30684e83e08d114a
Balijawa Hussein <balijawahussein@gmail.com>
2025-07-20 19:25:12 +0300

    UPDATE : TICKET UI

--- Client/src/pages/bookedFlights/flights.page.jsx
+++ Client/src/pages/bookedFlights/flights.page.jsx
@@ -1,12 +1,19 @@
 import './flights.style.scss';
 import BookedTicket from '../../components/BookedFlightTicket/ticket.component';
+import BackBTN from '../../components/features/BackButton/BackBTN';
+import { ArrowDown } from 'lucide-react';
 
 function BookedFlights() {
   return (
     <section className='tickets-page'>
       <div className="tickets-actions">
-        <div className="back-arrow"></div>
-        <div className="download-btn"></div>
+        <div className="back-arrow">
+          <BackBTN />
+        </div>
+        <div className="download-btn">
+          <ArrowDown size={20} color='#f7f7f7' />
+          <h4>download</h4>
+        </div>
       </div>
       <BookedTicket />
       <button className='submmit-btn'>
