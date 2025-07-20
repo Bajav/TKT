79ae7cc522089cf0836ade9c3545ff2b4b9d26cc
Balijawa Hussein <balijawahussein@gmail.com>
2025-07-20 19:09:11 +0300

    UPDATE : UI

--- Client/src/pages/bookedFlights/flights.page.jsx
+++ Client/src/pages/bookedFlights/flights.page.jsx
@@ -1,6 +1,6 @@
 import './flights.style.scss';
 import BookedTicket from '../../components/BookedFlightTicket/ticket.component';
-// import TripDateContainer from '../../components/BookedFlightTicket/date-trip-container/tripdate.component';
+import TripDateContainer from '../../components/BookedFlightTicket/date-trip-container/tripdate.component';
 
 function BookedFlights() {
   return (
