5b7d235cbe22ef6e7ac790949d448f6495c8bb42
Balijawa Hussein <balijawahussein@gmail.com>
2025-07-20 18:49:06 +0300

    UPDATE : TICKET UI

--- Client/src/components/BookedFlightTicket/ticket.component.jsx
+++ Client/src/components/BookedFlightTicket/ticket.component.jsx
@@ -4,6 +4,7 @@ import TicketHeader from "../flightSearch/Ticket/ticketheader.component";
 import klmLogo from "../../assets/images/klmLogo.png";
 import border from "../../assets/icons/border-line.svg";
 // import Alert from "../features/Error&Sucess/alert.component";
+import TripDateContainer from "./date-trip-container/tripdate.component";
 import FlightFlag from "../flightarrival/flightflag.component";
 import { CircleAlert } from "lucide-react";
 
@@ -33,79 +34,93 @@ function BookedTicket() {
     className: undefined,
   };
   return (
-    <div className="booked-ticket">
-      <div className="booked-ticket-header">
-        <div className="airlinelogo">
-          <img src={klmLogo} />
-        </div>
-        <div className="airline-data">
-          <div className="flexed-text">
-            <h3>KL 920</h3>
-            <h3>A232/FL</h3>
+    <div className="tickets-container">
+      <TripDateContainer tripType="outbound" dates="Wednesday ,26, jun 2025" />
+      <div className="booked-ticket">
+        <div className="booked-ticket-header">
+          <div className="airlinelogo">
+            <img src={klmLogo} />
           </div>
-          <div className="flexed-text">
-            <h3>travel date </h3>
-            <h3>23-03-2025</h3>
+          <div className="airline-data">
+            <div className="flexed-text">
+              <h3>KL 920</h3>
+              <h3>A232/FL</h3>
+            </div>
+            <div className="flexed-text">
+              <h3>travel date </h3>
+              <h3>23-03-2025</h3>
+            </div>
+            <div className="flexed-text">
+              <h3>booking ref</h3>
+              <h3>kl/xynfte</h3>
+            </div>
           </div>
-          <div className="flexed-text">
-            <h3>booking ref</h3>
-            <h3>kl/xynfte</h3>
+        </div>
+        <TicketHeader
+          styles={defaults}
+          originCode="kla"
+          originCity="kampala, uganda"
+          originTime="08:12:00"
+          originTerminal="terminal : 1e"
+          arrowColor="#222"
+          destinationCode="dxb"
+          destinationCity="dubai,uae"
+          destinationTime="08:12:00"
+          destinationTerminal="terminal : 2b"
+        />
+        <div className="flight-details">
+          <div className="detail-flex">
+            <h3>passenger</h3>
+            <h3>balijawa hussein</h3>
+          </div>
+
+          <div className="detail-flex">
+            <h3>cabin class</h3>
+            <h3>economy</h3>
+          </div>
+          <div className="detail-flex small-width">
+            <h3>seat number</h3>
+            <h3>18A</h3>
+          </div>
+          <div className="detail-flex">
+            <h3>ticket number</h3>
+            <h3>FL/ETK 2123347218</h3>
+          </div>
+
+          <div className="detail-flex">
+            <h3>flight code</h3>
+            <h3>a380 KQC3</h3>
+          </div>
+
+          <div className="detail-flex small-width">
+            <h3>Terminal</h3>
+            <h3>T1</h3>
           </div>
         </div>
+        <div className="ticket-border">
+          <img src={border} />
+        </div>
+        <Barcode
+          height={barcodeOptions.height}
+          margin={barcodeOptions.margin}
+          displayValue={barcodeOptions.displayValue}
+          background={barcodeOptions.background}
+          lineColor={barcodeOptions.lineColor}
+          value="212334721921233492334233423211"
+        />
+        <FlightFlag
+          icon={
+            <CircleAlert
+              style={{ rotate: "180deg" }}
+              size={15}
+              color="#79CC77"
+            />
+          }
+          bgColor="#D3F3D5"
+          fontColor="#79CC77"
+          alertText="arrives the next day"
+        />
       </div>
-      <TicketHeader
-        styles={defaults}
-        originCode="kla"
-        originCity="kampala, uganda"
-        originTime="08:12:00"
-        originTerminal="terminal : 1e"
-        arrowColor="#222"
-        destinationCode="dxb"
-        destinationCity="dubai,uae"
-        destinationTime="08:12:00"
-        destinationTerminal="terminal : 2b"
-      />
-      <div className="flight-details">
-        <div className="detail-flex">
-          <h3>passenger</h3>
-          <h3>balijawa hussein</h3>
-        </div>
-
-        <div className="detail-flex">
-          <h3>cabin class</h3>
-          <h3>economy</h3>
-        </div>
-        <div className="detail-flex small-width">
-          <h3>seat number</h3>
-          <h3>18A</h3>
-        </div>
-        <div className="detail-flex">
-          <h3>ticket number</h3>
-          <h3>FL/ETK 2123347218</h3>
-        </div>
-
-        <div className="detail-flex">
-          <h3>flight code</h3>
-          <h3>a380 KQC3</h3>
-        </div>
-
-        <div className="detail-flex small-width">
-          <h3>Terminal</h3>
-          <h3>T1</h3>
-        </div>
-      </div>
-      <div className="ticket-border">
-        <img src={border} />
-      </div>
-      <Barcode
-        height={barcodeOptions.height}
-        margin={barcodeOptions.margin}
-        displayValue={barcodeOptions.displayValue}
-        background={barcodeOptions.background}
-        lineColor={barcodeOptions.lineColor}
-        value="212334721921233492334233423211"
-      />
-      <FlightFlag icon={<CircleAlert style={{rotate:"180deg"}} size={15} color="#79CC77"/>} bgColor="#D3F3D5" fontColor="#79CC77" alertText="arrives the next day"/>
     </div>
   );
 }
--- Client/src/pages/bookedFlights/flights.page.jsx
+++ Client/src/pages/bookedFlights/flights.page.jsx
@@ -1,11 +1,10 @@
 import './flights.style.scss';
 import BookedTicket from '../../components/BookedFlightTicket/ticket.component';
-import TripDateContainer from '../../components/BookedFlightTicket/date-trip-container/tripdate.component';
+// import TripDateContainer from '../../components/BookedFlightTicket/date-trip-container/tripdate.component';
 
 function BookedFlights() {
   return (
     <section className='tickets-page'>
-      <TripDateContainer tripType="outbound" dates="Wednesday ,26, jun 2025"/>
       <BookedTicket />
     </section>
   )
