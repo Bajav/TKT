feb73c207c55f4527ca7e299d4fd500db2c5bc64
Balijawa Hussein <balijawahussein@gmail.com>
2025-07-24 06:46:53 +0300

    UPDATE: USER IS PERSISTENT

--- Client/src/Controllers/finduser.controller.jsx
+++ Client/src/Controllers/finduser.controller.jsx
@@ -7,7 +7,7 @@ import { db } from "../Utils/firestore.utils";
 const getUserByUID = async (uid) => {
   const userRef = doc(db, "user", uid);
   const userSnap = await getDoc(userRef);
-  console.log(userSnap);
+  // console.log(userSnap);
   if (userSnap.exists()) {
     return userSnap.data();
   } else {
