14249862f6e534bc0bda93d5fa669758475fbcee
Balijawa Hussein <balijawahussein@gmail.com>
2025-10-21 07:32:40 +0300

    UPDATE : MULTICITY FLIGHT FORMS

--- Client/src/components/flightSearch/flightsForm/flightsFormInput.jsx
+++ Client/src/components/flightSearch/flightsForm/flightsFormInput.jsx
@@ -17,8 +17,15 @@ import "./flightsForm.scss";
 
 function FlightsForm() {
   // contexts
-  const { setFormData, setIataCodes, setFlightResults,setFilteredFlights,setBrandedUpSell,setBookedFlight, setAlert } =
-    useContext(FlightContext);
+  const {
+    setFormData,
+    setIataCodes,
+    setFlightResults,
+    setFilteredFlights,
+    setBrandedUpSell,
+    setBookedFlight,
+    setAlert,
+  } = useContext(FlightContext);
   const { userLocation } = useContext(LocationContext);
 
   const location = useLocation();
@@ -56,18 +63,16 @@ function FlightsForm() {
         console.error("Error fetching flight data:", error);
       }
     };
-    const resetState =()=>
-      {
-        setFilteredFlights([]);
-        setFlightResults([]);
-        setBrandedUpSell([]);
-        setBookedFlight([]);
-        console.log("state reseted");
-      }
+    const resetState = () => {
+      setFilteredFlights([]);
+      setFlightResults([]);
+      setBrandedUpSell([]);
+      setBookedFlight([]);
+      console.log("state reseted");
+    };
     fetchData();
     resetState();
   }, []);
-  
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -126,7 +131,7 @@ function FlightsForm() {
     };
     setFormData(formData);
     setAlert(false);
-    navigate("/flights/results",{state:formData});
+    navigate("/flights/results", { state: formData });
   };
 
   const handleSwitch = (e) => {
@@ -146,7 +151,7 @@ function FlightsForm() {
   // const [calenderType, setIsRangeEnabled] = useState(false);
 
   const handleDepartureSelect = (date) => {
-    setDepartureDate(date); 
+    setDepartureDate(date);
     setInputs((prev) => ({
       ...prev,
       departureDate: date,
@@ -176,172 +181,219 @@ function FlightsForm() {
   return (
     <div>
       {location.pathname === "/flights" && (
-        <div className="cutout-box">
-          <form onSubmit={handleSubmit}>
-            <div className="flightSearch">
-              <div className="flex-option">
-                <ClickOption
-                  key="oneWay"
-                  className="trav"
-                  labelName="oneWay"
-                  label="one way"
-                  checkName="flightType"
-                  changeFunc={() => handleTripTypeClick("oneWay")}
-                  checkedName={inputs.flightType === "oneWay"}
-                  click={() => handleTripTypeClick("oneWay")}
-                  isActive={inputs.flightType === "oneWay"}
-                />
+        <div className="search-container">
+          <div className="cutout-box">
+            <form onSubmit={handleSubmit}>
+              <div className="flightSearch">
+                <div className="flex-option">
+                  <ClickOption
+                    key="oneWay"
+                    className="trav"
+                    labelName="oneWay"
+                    label="one way"
+                    checkName="flightType"
+                    changeFunc={() => handleTripTypeClick("oneWay")}
+                    checkedName={inputs.flightType === "oneWay"}
+                    click={() => handleTripTypeClick("oneWay")}
+                    isActive={inputs.flightType === "oneWay"}
+                  />
 
-                <ClickOption
-                  key="roundTrip"
-                  className="trav"
-                  labelName="roundTrip"
-                  label="round trip"
-                  checkName="flightType"
-                  changeFunc={() => handleTripTypeClick("roundTrip")}
-                  checkedName={inputs.flightType === "roundTrip"}
-                  click={() => handleTripTypeClick("roundTrip")}
-                  isActive={inputs.flightType === "roundTrip"}
-                />
+                  <ClickOption
+                    key="roundTrip"
+                    className="trav"
+                    labelName="roundTrip"
+                    label="round trip"
+                    checkName="flightType"
+                    changeFunc={() => handleTripTypeClick("roundTrip")}
+                    checkedName={inputs.flightType === "roundTrip"}
+                    click={() => handleTripTypeClick("roundTrip")}
+                    isActive={inputs.flightType === "roundTrip"}
+                  />
 
-                <ClickOption
-                  key="multiCity"
-                  className="trav"
-                  labelName="multiCity"
-                  label="multi city"
-                  checkName="flightType"
-                  changeFunc={() => handleTripTypeClick("multiCity")}
-                  checkedName={inputs.flightType === "multiCity"}
-                  click={() => handleTripTypeClick("multiCity")}
-                  isActive={inputs.flightType === "multiCity"}
-                />
-                <div
-                  className="actives"
-                  style={{
-                    left:
-                      inputs.flightType === "oneWay"
-                        ? "1px"
-                        : inputs.flightType === "roundTrip"
-                        ? "140px"
-                        : "270px",
-                        width:inputs.flightType === "oneWay"
-                        ? "120px"
-                        : inputs.flightType === "roundTrip"
-                        ? "120px"
-                        : "132px",
-                  }}
-                />
-              </div>
-
-              <div className="flightInputs">
-                <FlightSearchInput
-                  classOne="flexInput"
-                  labelFor="Origin"
-                  label="Origin"
-                  placeholder="Input place of origin"
-                  InputName="origin"
-                  change={handleChange}
-                  value={inputs.origin}
-                />
-                <motion.button
-                  initial={{ rotate: 0 }}
-                  animate={
-                    clicked ? { rotate: 360, type: "spring" } : { rotate: 0 }
-                  }
-                  className="switchBtn"
-                  onClick={handleSwitch}
-                >
-                  <img src={RoundTripIcon} alt="" />
-                </motion.button>
-                <div className="newFlex">
-                  <FlightSearchInput
-                    classOne="flexInput inputTwo"
-                    labelFor="Destination"
-                    label="Destination"
-                    placeholder="Input place of destination"
-                    InputName="destination"
-                    change={handleChange}
-                    value={inputs.destination}
+                  <ClickOption
+                    key="multiCity"
+                    className="trav"
+                    labelName="multiCity"
+                    label="multi city"
+                    checkName="flightType"
+                    changeFunc={() => handleTripTypeClick("multiCity")}
+                    checkedName={inputs.flightType === "multiCity"}
+                    click={() => handleTripTypeClick("multiCity")}
+                    isActive={inputs.flightType === "multiCity"}
+                  />
+                  <div
+                    className="actives"
+                    style={{
+                      left:
+                        inputs.flightType === "oneWay"
+                          ? "1px"
+                          : inputs.flightType === "roundTrip"
+                          ? "140px"
+                          : "270px",
+                      width:
+                        inputs.flightType === "oneWay"
+                          ? "120px"
+                          : inputs.flightType === "roundTrip"
+                          ? "120px"
+                          : "132px",
+                    }}
                   />
                 </div>
+
+                <div className="flightInputs">
+                  <FlightSearchInput
+                    classOne="flexInput"
+                    labelFor="Origin"
+                    label="Origin"
+                    placeholder="Input place of origin"
+                    InputName="origin"
+                    change={handleChange}
+                    value={inputs.origin}
+                  />
+                  <motion.button
+                    initial={{ rotate: 0 }}
+                    animate={
+                      clicked ? { rotate: 360, type: "spring" } : { rotate: 0 }
+                    }
+                    className="switchBtn"
+                    onClick={handleSwitch}
+                  >
+                    <img src={RoundTripIcon} alt="" />
+                  </motion.button>
+                  <div className="newFlex">
+                    <FlightSearchInput
+                      classOne="flexInput inputTwo"
+                      labelFor="Destination"
+                      label="Destination"
+                      placeholder="Input place of destination"
+                      InputName="destination"
+                      change={handleChange}
+                      value={inputs.destination}
+                    />
+                  </div>
+                </div>
+              </div>
+              <img className="lines" src={line} />
+              <FlightCalendar
+                onDateSelect={handleDepartureSelect}
+                onRangeSelect={handleReturnRangeSelect}
+                isRangePicker={isRangeEnabled}
+                placeholder="Select departure date"
+              />
+              <div className="seatType">
+                <div className="passenger-selector">
+                  <div className="dropdown-header" onClick={toggleDropdown}>
+                    <span>Passengers : {getTotalPassengers()}</span>
+                    <span className="arrow">{isDropdownOpen ? "▲" : "▼"}</span>
+                  </div>
+
+                  {isDropdownOpen && (
+                    <div className="dropdown-content">
+                      {["adults", "children", "infants"].map((type) => (
+                        <div className="passenger-type" key={type}>
+                          <div className="passenger-label">
+                            {type.charAt(0).toUpperCase() + type.slice(1)}
+                          </div>
+                          <div className="pax">
+                            <button
+                              onClick={(e) => {
+                                e.preventDefault();
+                                decrement(type);
+                              }}
+                              disabled={
+                                type === "adults"
+                                  ? passengers[type] <= 1
+                                  : passengers[type] <= 0
+                              }
+                            >
+                              -
+                            </button>
+                            <span>{passengers[type]}</span>
+                            <button
+                              onClick={(e) => {
+                                e.preventDefault();
+                                increment(type);
+                              }}
+                            >
+                              +
+                            </button>
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  )}
+                </div>
+                <div className="seatClass">
+                  <label htmlFor="seatClass"> Seat Class</label>
+                  <select
+                    name="seatClass"
+                    id="seatClass"
+                    onChange={handleChange}
+                    value={inputs.seatClass}
+                  >
+                    <option value="ECONOMY">Economy</option>
+                    <option value="PREMIUM_ECONOMY">Economy Premium</option>
+                    <option value="BUSINESS">Business</option>
+                    <option value="FIRST">First Class</option>
+                  </select>
+                </div>
+
+                {/* <div className="seat">
+                    {["economy", "business", "first class"].map((type) => (
+                     <li>{type}</li>
+                    ))}
+                  </div> */}
+              </div>
+
+              <button className="btn-submit" type="submit">
+                Search Flights
+              </button>
+            </form>
+          </div>
+          <div className="multicity-flight">
+            <div className="multicity-header">
+              <h4>flight {2}</h4>
+              <button>add flight</button>
+            </div>
+            <div className="flightInputs">
+              <FlightSearchInput
+                classOne="flexInput"
+                labelFor="Origin"
+                label="Origin"
+                placeholder="Input place of origin"
+                InputName="origin"
+                change={handleChange}
+                value={inputs.origin}
+              />
+              <motion.button
+                initial={{ rotate: 0 }}
+                animate={
+                  clicked ? { rotate: 360, type: "spring" } : { rotate: 0 }
+                }
+                className="switchBtn"
+                onClick={handleSwitch}
+              >
+                <img src={RoundTripIcon} alt="" />
+              </motion.button>
+              <div className="newFlex">
+                <FlightSearchInput
+                  classOne="flexInput inputTwo"
+                  labelFor="Destination"
+                  label="Destination"
+                  placeholder="Input place of destination"
+                  InputName="destination"
+                  change={handleChange}
+                  value={inputs.destination}
+                />
               </div>
             </div>
-            <img className="lines" src={line} />
             <FlightCalendar
               onDateSelect={handleDepartureSelect}
               onRangeSelect={handleReturnRangeSelect}
               isRangePicker={isRangeEnabled}
               placeholder="Select departure date"
             />
-            <div className="seatType">
-              <div className="passenger-selector">
-                <div className="dropdown-header" onClick={toggleDropdown}>
-                  <span>Passengers : {getTotalPassengers()}</span>
-                  <span className="arrow">{isDropdownOpen ? "▲" : "▼"}</span>
-                </div>
-
-                {isDropdownOpen && (
-                  <div className="dropdown-content">
-                    {["adults", "children", "infants"].map((type) => (
-                      <div className="passenger-type" key={type}>
-                        <div className="passenger-label">
-                          {type.charAt(0).toUpperCase() + type.slice(1)}
-                        </div>
-                        <div className="pax">
-                          <button
-                            onClick={(e) => {
-                              e.preventDefault();
-                              decrement(type);
-                            }}
-                            disabled={
-                              type === "adults"
-                                ? passengers[type] <= 1
-                                : passengers[type] <= 0
-                            }
-                          >
-                            -
-                          </button>
-                          <span>{passengers[type]}</span>
-                          <button
-                            onClick={(e) => {
-                              e.preventDefault();
-                              increment(type);
-                            }}
-                          >
-                            +
-                          </button>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                )}
-              </div>
-              <div className="seatClass">
-                <label htmlFor="seatClass"> Seat Class</label>
-                <select
-                  name="seatClass"
-                  id="seatClass"
-                  onChange={handleChange}
-                  value={inputs.seatClass}
-                >
-                  <option value="ECONOMY">Economy</option>
-                  <option value="PREMIUM_ECONOMY">Economy Premium</option>
-                  <option value="BUSINESS">Business</option>
-                  <option value="FIRST">First Class</option>
-                </select>
-              </div>
-
-              {/* <div className="seat">
-                    {["economy", "business", "first class"].map((type) => (
-                     <li>{type}</li>
-                    ))}
-                  </div> */}
-            </div>
-
-            <button className="btn-submit" type="submit">
-              Search Flights
-            </button>
-          </form>
+          </div>
         </div>
       )}
       {/* <Outlet /> */}
