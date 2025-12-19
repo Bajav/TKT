a2ca723b2fce16a485f5212272706426a76aa4d2
Balijawa Hussein <balijawahussein@gmail.com>
2025-12-20 01:39:09 +0300

    UPDATE : UPDATING USER SESSIONS

--- Server/Routes/cookie.routes.js
+++ Server/Routes/cookie.routes.js
@@ -1,10 +1,10 @@
 import {Router} from 'express';
-import { getCookies,findCookies } from '../Controllers/cookie.controller.js';
+import { storeUser,findCookies } from '../Controllers/cookie.controller.js';
 
 const router = Router();
 
 router.get('/',findCookies);
-router.post('/',getCookies);
+router.post('/',storeUser);
 
 
 export default router;
