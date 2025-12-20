b2fcde7f35f45f80df06b6b21fbf6ad31ae10884
Balijawa Hussein <balijawahussein@gmail.com>
2025-12-20 17:41:47 +0300

    UPDATE : SEARCH REDUCER

--- Client/src/components/context/flightSearch.context.jsx
+++ Client/src/components/context/flightSearch.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useReducer } from "react";
 
 export const FlightContext = createContext({
   flightSearch: null,
@@ -25,18 +25,52 @@ export const FlightContext = createContext({
   setFilteredFlights: () => {},
 });
 
+const ACTIONS = {
+  SET_FORM_DATA: "SET_FORM_DATA ",
+  SET_IATA_CODES: "SET_IATA_CODES",
+  SET_AIRLINE_DATA: "SET_AIRLINE_DATA",
+  SET_FLIGHT_RESULTS: "SET_FLIGHT_RESULTS",
+  SET_SELECTED_FLIGHTS: "SET_SELECTED_FLIGHTS",
+  SET_FILTERED_FLIGHTS: "SET_FILTERED_FLIGHTS",
+  SET_BRANDED_UPSELL: "SET_BRANDED_UPSELL",
+  SET_BOOKED_FLIGHT: "SET_BOOKED_FLIGHT",
+  SET_UPSELL_ERROR: "SET_UPSELL_ERROR",
+  SET_LAST_FLIGHT: "SET_LAST_FLIGHT",
+  SET_ALERT: "SET_ALERT",
+};
+
+const flightReducer =(state, action)=>{
+  switch(action.type){
+    case ACTIONS.SET_FORM_DATA : return{
+      ...state, flightSearch : action.payload
+    };
+    // case ACTIONS.SET_FORM_DATA : return{}
+    case ACTIONS.SET_IATA_CODES : return{
+      ...state, iataCodes :action.payload
+    }
+    case ACTIONS.SET_AIRLINE_DATA : return{
+      ...state, airlineData: action.payload
+    }
+    case ACTIONS.SET_ALERT : return{}
+    case ACTIONS.SET_BOOKED_FLIGHT : return{}
+    case ACTIONS.SET_BRANDED_UPSELL : return{}
+    case ACTIONS.SET_SELECTED_FLIGHTS : return{}
+    case ACTIONS.SET_UPSELL_ERROR : return{}
+  }
+}
+
 export const FlightSearchProvider = ({ children }) => {
-  const [flightSearch, setFormData] = useState(null);
-  const [iataCodes, setIataCodes] = useState([]);
-  const [airlineData, setAirlineData] = useState([]);
-  const [flightResults, setFlightResults] = useState([]);
-  const [selectedFlight, setSelectFlight] = useState([]);
-  const [bookedFlight, setBookedFlight] = useState([]);
-  const [brandedUpSell, setBrandedUpSell] = useState([]);
-  const [upsellError, setUpsellError] = useState("");
-  const [lastFlight, setlastFlight] = useState([]);
-  const [alert, setAlert] = useState(false);
-  const [filteredFlights, setFilteredFlights] = useState([]);
+  // const [flightSearch, setFormData] = useState(null);
+  // const [iataCodes, setIataCodes] = useState([]);
+  // const [airlineData, setAirlineData] = useState([]);
+  // const [flightResults, setFlightResults] = useState([]);
+  // const [selectedFlight, setSelectFlight] = useState([]);
+  // const [bookedFlight, setBookedFlight] = useState([]);
+  // const [brandedUpSell, setBrandedUpSell] = useState([]);
+  // const [upsellError, setUpsellError] = useState("");
+  // const [lastFlight, setlastFlight] = useState([]);
+  // const [alert, setAlert] = useState(false);
+  // const [filteredFlights, setFilteredFlights] = useState([]);
   const value = {
     flightSearch,
     setFormData,
@@ -61,6 +95,7 @@ export const FlightSearchProvider = ({ children }) => {
     filteredFlights,
     setFilteredFlights,
   };
+
   return (
     <FlightContext.Provider value={value}>{children}</FlightContext.Provider>
   );
