f55ae7af925951b02251d515da8759fc546bee6d
Balijawa Hussein <balijawahussein@gmail.com>
2026-01-01 03:23:57 +0300

    UPDATE : HOTELS UI

--- Client/src/components/Hotels/HotelCard/hotelcard.component.jsx
+++ Client/src/components/Hotels/HotelCard/hotelcard.component.jsx
@@ -1,1 +1,56 @@
-import './hotelcard.styles.scss';
+import "./hotelcard.styles.scss";
+
+const HotelCard = ({
+  hotelName,
+  city,
+  country,
+  description,
+  image,
+  mainPrice,
+  pricePerNight,
+  rating,
+  reviewCount,
+  amenities = [],
+}) => {
+  return (
+    <div className="hotel-card">
+      <div className="hotel-img">
+        <img src={image} alt={hotelName} />
+      </div>
+
+      <div className="hotel-data">
+        <div className="hotel-header">
+          <div className="hotel-name-country">
+            <h1>{hotelName}</h1>
+            <h4>{city}, {country}</h4>
+          </div>
+
+          <div className="reviews">
+            <div className="stars">
+              <h5>{rating}</h5>
+              <div className="rate-stars" />
+            </div>
+            <h4>{reviewCount} reviews</h4>
+          </div>
+        </div>
+
+        <p>{description}</p>
+
+        <div className="amenities-price">
+          <div className="amenities">
+            {amenities.slice(0, 8).map((item) => (
+              <span key={item}>{item}</span>
+            ))}
+          </div>
+
+          <div className="price">
+            <h2>€{mainPrice}</h2>
+            <h5>€{pricePerNight} per night</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HotelCard;
--- Client/src/pages/Hotels/hotels.jsx
+++ Client/src/pages/Hotels/hotels.jsx
@@ -1,12 +1,26 @@
-import './hotels.styles.scss';
+import "./hotels.styles.scss";
 import HotelForm from "../../components/Hotels/FormInputs/form/form.component";
-import LocationHeader from '../../components/Header/header';
-function Hotels (){
-    return(
-       <div className='home'>
-        <LocationHeader />
-        <HotelForm />
-       </div>
-    );
+import LocationHeader from "../../components/Header/header";
+import HotelCard from "../../components/Hotels/HotelCard/hotelcard.component";
+function Hotels() {
+  return (
+    <div className="home">
+      <LocationHeader />
+      <HotelForm />
+      <HotelCard
+        hotelName={"Hôtel Ritz"}
+        city={"Paris"}
+        country={"France"}
+        description={
+          "Ritz Paris is a renowned 5-star luxury hotel located in the heart of Paris,France. Famous for its classic architecture, refined service."
+        }
+        image={"SD"}
+        mainPrice={"1800"}
+        pricePerNight={100}
+        rating={9.0}
+        reviewCount={25}
+      />
+    </div>
+  );
 }
 export default Hotels;
