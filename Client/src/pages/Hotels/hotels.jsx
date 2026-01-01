707844c2ba30437e39ada6cd10cc43980ca885a2
Balijawa Hussein <balijawahussein@gmail.com>
2026-01-01 04:17:25 +0300

    UPDATE : HOTELS UI

--- Client/src/pages/Hotels/hotels.jsx
+++ Client/src/pages/Hotels/hotels.jsx
@@ -2,24 +2,31 @@ import "./hotels.styles.scss";
 import HotelForm from "../../components/Hotels/FormInputs/form/form.component";
 import LocationHeader from "../../components/Header/header";
 import HotelCard from "../../components/Hotels/HotelCard/hotelcard.component";
+import hotelJson from "../../data/hotelsJson.json";
+import hotelImg from '../../assets/images/hotelImg.jpg'
 function Hotels() {
   return (
     <div className="home">
       <LocationHeader />
       <HotelForm />
-      <HotelCard
-        hotelName={"Hôtel Ritz"}
-        city={"Paris"}
-        country={"France"}
-        description={
-          "Ritz Paris is a renowned 5-star luxury hotel located in the heart of Paris,France. Famous for its classic architecture, refined service."
-        }
-        image={"SD"}
-        mainPrice={"1800"}
-        pricePerNight={100}
-        rating={9.0}
-        reviewCount={25}
-      />
+      {hotelJson.hotels.hotels.map((hotel) => {
+        const { name, destinationName,categoryName,minRate } = hotel;
+        return (
+          <HotelCard
+            hotelName={name}
+            city={"Paris"}
+            country={destinationName}
+            description={
+              "Ritz Paris is a renowned 5-star luxury hotel located in the heart of Paris,France. Famous for its classic architecture, refined service."
+            }
+            image={hotelImg}
+            mainPrice={minRate}
+            pricePerNight={100}
+            rating={9.0}
+            reviewCount={25}
+          />
+        );
+      })}
     </div>
   );
 }
