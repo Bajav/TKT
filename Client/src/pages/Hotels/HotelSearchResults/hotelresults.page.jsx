2631bdc55f7b67b2254e3bca5192e7267eb77ebe
Balijawa Hussein <balijawahussein@gmail.com>
2026-01-02 02:26:31 +0300

    UPDATE : UI

--- Client/src/pages/Hotels/HotelSearchResults/hotelresults.page.jsx
+++ Client/src/pages/Hotels/HotelSearchResults/hotelresults.page.jsx
@@ -4,13 +4,21 @@ import "./hotelresults.styles.scss";
 import hotelJson from "../../../data/hotelsJson.json";
 import HotelCard from "../../../components/Hotels/HotelCard/hotelcard.component";
 import hotelImg from "../../../assets/images/hotelImg.jpg";
+import { SlidersHorizontal } from "lucide-react";
+
 function HotelResults() {
   return (
     <main className="hotel-results">
       <div className="results-header">
         <button className="cancelBtn">cancel</button>
       </div>
-      <h1>hotel results</h1>
+          <h1 className="results-head">hotel results</h1>
+      <div className="results-header">
+        <div className="filter">
+            <SlidersHorizontal color="#222" size={8}/>
+            <h5>filters</h5>
+        </div>
+      </div>
       {hotelJson.hotels.hotels.map((hotel, index) => {
         const { name, destinationName, categoryName, minRate } = hotel;
         return (
