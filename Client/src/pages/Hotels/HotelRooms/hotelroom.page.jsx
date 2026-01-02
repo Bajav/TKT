acc30caeff57a233d8d00def165c43bb9a3d93d0
Balijawa Hussein <balijawahussein@gmail.com>
2026-01-02 03:49:49 +0300

    UPDATE : UI

--- Client/src/pages/Hotels/HotelRooms/hotelroom.page.jsx
+++ Client/src/pages/Hotels/HotelRooms/hotelroom.page.jsx
@@ -1,13 +1,25 @@
-import './hotelroom.styles.scss';
-import hotelJson from '../../../data/hotelsJson.json';
-import HotelCard from '../../../components/Hotels/HotelCard/hotelcard.component';
-
+import "./hotelroom.styles.scss";
+import hotelJson from "../../../data/hotelsJson.json";
+import HotelCard from "../../../components/Hotels/HotelCard/hotelcard.component";
+import { useNavigate } from "react-router-dom";
+import BackBTN from "../../../components/features/BackButton/BackBTN";
+import hotelImgTwo from "../../../assets/images/hotelTwo.jpg";
 function HotelRoom() {
+  const navigate = useNavigate();
+  const backBtn = () => {
+    // setFlightResults([]);
+    // setFilteredFlights([]);
+    navigate("/searchhotels/results");
+  };
   return (
-    <section className='hotel-results'>
-        this is rooms
+    <section className="hotel-results">
+      <BackBTN onClick={backBtn} btnName="back" />
+      <div className="rooms-header">
+        <img src={hotelImgTwo} alt="" />
+         <div className="overlay" />
+      </div>
     </section>
-  )
+  );
 }
 
 export default HotelRoom;
