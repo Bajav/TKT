ccd7bd16d98b40cd041b0afb9fbdc2f540d0d10a
Balijawa Hussein <balijawahussein@gmail.com>
2026-01-04 20:43:27 +0300

    UPDATE : UI

--- Client/src/pages/Hotels/HotelRooms/hotelroom.page.jsx
+++ Client/src/pages/Hotels/HotelRooms/hotelroom.page.jsx
@@ -14,26 +14,31 @@ function HotelRoom() {
     navigate("/searchhotels/results");
   };
   return (
-    <section className="hotel-results">
+    <section className="hotel-rooms">
       <BackBTN onClick={backBtn} btnName="back" />
       <div className="rooms-header">
         <img src={hotelImgTwo} alt="" />
         <div className="overlay" />
       </div>
+      <div className="flex-rates">
       <Rates rateNum={3} reviewCount={30} rating={4} />
+      </div>
       <div className="main-results">
         <h4>The Beverly Hills Hotel</h4>
-        <p>The Beverly Hills Hotel is a legendary 5-star luxury hotel located in Beverly Hills,
-California, United States. Known as “The Pink Palace,” it offers world-class
-accommodation, fine dining, and premium service</p>
+        <p>
+          The Beverly Hills Hotel is a legendary 5-star luxury hotel located in
+          Beverly Hills, California, United States. Known as “The Pink Palace,”
+          it offers world-class accommodation, fine dining, and premium service
+        </p>
       </div>
       <div className="facilites-container">
-      <div className="facilites">
-        <div className="facility">
-          <AirVent size={20} color="#2f7bc8 "/>
-          <h4>Air conditioning</h4>
+        <h4 className="facilities-header">facilites</h4>
+        <div className="facilites">
+          <div className="facility">
+            <AirVent size={20} color="#2f7bc8" />
+            <h4>Air conditioning</h4>
+          </div>
         </div>
-      </div>
       </div>
     </section>
   );
