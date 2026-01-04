50ac5cde68c82cf2c1eca79cbec053bbc5c6183e
Balijawa Hussein <balijawahussein@gmail.com>
2026-01-04 22:15:47 +0300

    UPDATE : ui

--- Client/src/pages/Hotels/HotelRooms/hotelroom.page.jsx
+++ Client/src/pages/Hotels/HotelRooms/hotelroom.page.jsx
@@ -21,7 +21,7 @@ function HotelRoom() {
         <div className="overlay" />
       </div>
       <div className="flex-rates">
-      <Rates rateNum={3} reviewCount={30} rating={4} />
+        <Rates rateNum={3} reviewCount={30} rating={4} />
       </div>
       <div className="main-results">
         <h4>The Beverly Hills Hotel</h4>
@@ -30,41 +30,44 @@ function HotelRoom() {
           Beverly Hills, California, United States. Known as “The Pink Palace,”
           it offers world-class accommodation, fine dining, and premium service
         </p>
-           <div className="facilites-container">
-        <h4 className="facilities-header">facilites</h4>
-        <div className="facilites">
-          <div className="facility">
-            <AirVent size={20} color="#2f7bc8" />
-            <h4>Air conditioning</h4>
-          </div>
-        </div>
-      </div>
-      <div className="hotel-reviews">
-        <h4>hotel reviews</h4>
-        <div className="reviews-container">
-          <div className="review-card">
-            <div className="review-header">
-              <div className="review-icon"></div>
-              <h3>hussein balijawa</h3>
+        <div className="facilites-container">
+          <h4 className="facilities-header">facilites</h4>
+          <div className="facilites">
+            <div className="facility">
+              <AirVent size={20} color="#2f7bc8" />
+              <h4>Air conditioning</h4>
             </div>
           </div>
         </div>
-      </div>
-      <div className="rooms-container">
-        <h2>rooms available</h2>
-        <div className="rooms">
-          <div className="room">
-          <div className="images">
-
-          </div>
-          <div className="room-header">
-            <h1>Classic Room</h1>
-            <h3>Luxury Collection</h3>
-            <h4>No refund</h4>
-          </div>
+        <div className="hotel-reviews">
+          <h4>hotel reviews</h4>
+          <div className="reviews-container">
+            <div className="review-card">
+              <div className="review-header">
+                <div className="review-icon"></div>
+                <h3>hussein balijawa</h3>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+        <div className="rooms-container">
+          <h2>rooms available</h2>
+          <div className="rooms">
+            <div className="room">
+              <div className="images"></div>
+              <div className="data-side">
+                <div className="room-header">
+                  <h1>Classic Room</h1>
+                  <h3>Luxury Collection</h3>
+                  <h4>No refund</h4>
+                </div>
+                <div className="other-info">
+                  <h4>$300</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
