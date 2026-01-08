3f697cf1a3d31f46dc5fa064a6adc5f2f3d7c355
Balijawa Hussein <balijawahussein@gmail.com>
2026-01-08 12:44:55 +0300

    UPDATE : HOTEL CONTEXT STATE

--- Client/src/components/context/hotels.contenxt.jsx
+++ Client/src/components/context/hotels.contenxt.jsx
@@ -7,14 +7,17 @@ export const HotelContext = createContext({
   hotelInfo: null,
   setHotelInfo: () => {},
   hotelError: null,
+  hotelImages: null,
   setHotelJson: () => {},
   setHotelSearchData: () => {},
   setSelectedHotel: () => {},
   setHotelError: () => {},
+  setHotelImages: () => {},
 });
 
 export const HotelActions = {
   SET_HOTEL_INFO: "SET_HOTEL_INFO",
+  SET_HOTEL_IMAGES: "SET_HOTEL_IMAGES",
   SET_HOTEL_JSON: "SET_HOTEL_JSON",
   SET_SELECTED_HOTEL: "SET_SELECTED_HOTEL",
   SET_HOTEL_SEARCH_DATA: "SET_HOTEL_SEARCH_DATA",
@@ -36,6 +39,8 @@ const HotelReducer = (state, action) => {
       return { ...state, hotelError: action.payload };
    case HotelActions.SET_HOTEL_INFO:
       return { ...state, hotelInfo: action.payload };
+        case HotelActions.SET_HOTEL_IMAGES:
+      return { ...state, hotelImages: action.payload };
     default:
       return state;
   }
@@ -47,6 +52,7 @@ const INITIAL_STATE = {
   selectedHotel: null,
   hotelError: null,
   hotelInfo: null,
+  hotelImages: null,
 };
 
 export const HotelProvider = ({ children }) => {
@@ -66,12 +72,16 @@ export const HotelProvider = ({ children }) => {
   const setHotelError = (data) =>
     dispatch({ type: HotelActions.SET_HOTEL_ERROR, payload: data });
 
+  const setHotelImages = (data) =>
+    dispatch({ type: HotelActions.SET_HOTEL_IMAGES, payload: data });
   const value = {
     hotelJson: state.hotelJson,
     hotelSearchData: state.hotelSearchData,
     selectedHotel: state.selectedHotel,
     hotelError: state.hotelError,
     hotelInfo: state.hotelInfo,
+    hotelImages: state.hotelImages,
+    setHotelImages,
     setHotelInfo,
     setHotelJson,
     setHotelSearchData,
