b933c8d73217892b6a544116f794448bb53fe0dc
Balijawa Hussein <balijawahussein@gmail.com>
2026-01-25 04:15:37 +0300

    UUPDATE : UI

--- Client/src/components/Hotels/RecentSeaches/recentsearch.component.jsx
+++ Client/src/components/Hotels/RecentSeaches/recentsearch.component.jsx
@@ -1,6 +1,10 @@
 import "./recentsearch.styles.scss";
-import recentSearches from '../../../data/recents.data.json'
-
+import recentSearches from "../../../data/recents.data.json";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectFade, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
 const Recentsearch = () => {
   return (
     <div className="search-items">
@@ -19,4 +23,36 @@ const Recentsearch = () => {
   );
 };
 
-export default Recentsearch;
+const Swipers = () => {
+  return (
+    <Swiper
+      direction={"vertical"}
+      effect={"fade"}
+      loop={true}
+      speed={800}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      modules={[Autoplay, Pagination, EffectFade]}
+      pagination={{
+        clickable: true,
+      }}
+      centeredSlides={true}
+      slidesPerView={1}
+      className="mySwiper"
+    >
+      {recentSearches.map((item, index) => (
+        <SwiperSlide className="slides-item" key={item.id ?? index}>
+          <img
+            className="slide-image"
+            src={item.imgUrl ?? ""}
+            alt={item.placeName ?? "recent search"}
+            loading="lazy"
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+export { Recentsearch, Swipers };
