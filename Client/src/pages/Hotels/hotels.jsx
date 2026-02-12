32acbc0f7919a7c799e8214001814d7b1fe3c79a
Balijawa Hussein <balijawahussein@gmail.com>
2026-02-12 23:13:17 +0300

    UPDATE : UI

--- Client/src/pages/Hotels/hotels.jsx
+++ Client/src/pages/Hotels/hotels.jsx
@@ -1,10 +1,10 @@
 import "./hotels.styles.scss";
 import HotelForm from "../../components/Hotels/FormInputs/form/form.component";
-import {Recentsearch} from "../../components/Hotels/RecentSeaches/recentsearch.component";
+import { Recentsearch } from "../../components/Hotels/RecentSeaches/recentsearch.component";
 import LocationHeader from "../../components/Header/header";
 import { Outlet, useLocation } from "react-router-dom";
 import { Fragment } from "react";
-import hotelSearch from "../../data/searchdata/hotelsearch.json"
+import hotelSearch from "../../data/searchdata/hotelsearch.json";
 
 function Hotels() {
   const location = useLocation();
@@ -16,7 +16,7 @@ function Hotels() {
   ];
 
   const shouldHideLayout = hideLayoutRoutes.includes(location.pathname);
-    const getSelected = (index) => {
+  const getSelected = (index) => {
     const selectedSearch = searchData[index];
     // setFormData(selectedSearch);
     console.log(index, selectedSearch);
@@ -27,23 +27,24 @@ function Hotels() {
         <div className="hotel-home">
           <LocationHeader />
           <HotelForm />
-               <div className="searched-wrapper">
-              {hotelSearch?.length > 0
-                ? hotelSearch?.map((item, index) => {
-                    return (
-                      <div
-                        className="search"
-                        onClick={getSelected}
-                        key={index}
-                      >
-                        <h5>location:{item.destination} </h5>
-                        <h5>rooms:{item.rooms} </h5>
-                        <h5>accommodationType :{item.accommodationType}</h5>
-                      </div>
-                    );
-                  })
-                : null}
-            </div>
+          <div className="searched-wrapper">
+            {hotelSearch?.length > 0
+              ? hotelSearch?.map((item, index) => {
+                  return (
+                    <div className="search" onClick={getSelected} key={index}>
+                      <h5>location:{item.destination} </h5>
+                      <h5>rooms:{item.rooms} </h5>
+                      <h5>accommodationType :{item.accommodationType}</h5>
+                    </div>
+                  );
+                })
+              : null}
+          </div>
+          <div className="visual-wrapper">
+            {/* <div className="hotel-items"></div>
+            <div className="hotel-items"></div>
+            <div className="hotel-items high"></div> */}
+          </div>
           {/* <Swipers/> */}
         </div>
       )}
